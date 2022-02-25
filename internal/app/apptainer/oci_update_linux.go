af81457c392dc7c22c7bfb3243762386c15c9839
David Trudgian <david.trudgian@sylabs.io>
2022-02-25 15:38:55 -0600

    oci: add conmon for detached flow
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/app/apptainer/oci_update_linux.go
+++ internal/app/apptainer/oci_update_linux.go
@@ -10,8 +10,8 @@
 package apptainer
 
 import (
-	"fmt"
-	"syscall"
+	"os"
+	"os/exec"
 
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
@@ -19,16 +19,16 @@ import (
 // OciUpdate updates container cgroups resources
 func OciUpdate(containerID string, args *OciArgs) error {
 	runcArgs := []string{
-		"--root=" + OciStateDir,
+		"--root", RuncStateDir,
 		"update",
 		"-r", args.FromFile,
 		containerID,
 	}
 
+	cmd := exec.Command(runc, runcArgs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdout
 	sylog.Debugf("Calling runc with args %v", runcArgs)
-	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
-		return fmt.Errorf("while calling runc: %w", err)
-	}
-
-	return nil
+	return cmd.Run()
 }
