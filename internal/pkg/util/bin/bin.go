d538e629f12fa48117a003d8a9797417f9f0ddde
David Trudgian <david.trudgian@sylabs.io>
2022-02-28 14:00:44 -0600

    oci: use FindBin for conmon, runc
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/pkg/util/bin/bin.go
+++ internal/pkg/util/bin/bin.go
@@ -47,7 +47,8 @@ func FindBin(name string) (path string, err error) {
 		return findOnPath(name, true)
 	// All other executables
 	// We will always search the user's PATH first for these
-	case "curl",
+	case "conmon",
+		"curl",
 		"debootstrap",
 		"dnf",
 		"fakeroot",
@@ -63,6 +64,7 @@ func FindBin(name string) (path string, err error) {
 		"pacstrap",
 		"rpm",
 		"rpmkeys",
+		"runc",
 		"squashfuse",
 		"squashfuse_ll",
 		"SUSEConnect",
