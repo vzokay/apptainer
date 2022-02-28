fed0ed9ef73aff2ca8bb052d4d31aef189737865
David Trudgian <david.trudgian@sylabs.io>
2022-02-28 14:00:44 -0600

    oci: use FindBin for conmon, runc
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/app/apptainer/oci_state_linux.go
+++ internal/app/apptainer/oci_state_linux.go
@@ -13,11 +13,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciState query container state
 func OciState(containerID string, args *OciArgs) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"state",
