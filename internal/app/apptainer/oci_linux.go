73df29ff532ec8bdc6181c5aa245a205e45b24c7
David Trudgian <david.trudgian@sylabs.io>
2022-03-09 09:23:36 -0600

    oci: simplify AttachStreams type
    As suggested in review the streams can be straight `io.Writer` /
    `io.Reader`.
    
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- internal/app/apptainer/oci_linux.go
+++ internal/app/apptainer/oci_linux.go
@@ -13,7 +13,6 @@
 package apptainer
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"os"
@@ -62,11 +61,11 @@ type OciArgs struct {
 // AttachStreams contains streams that will be attached to the container
 type AttachStreams struct {
 	// OutputStream will be attached to container's STDOUT
-	OutputStream io.WriteCloser
+	OutputStream io.Writer
 	// ErrorStream will be attached to container's STDERR
-	ErrorStream io.WriteCloser
+	ErrorStream io.Writer
 	// InputStream will be attached to container's STDIN
-	InputStream *bufio.Reader
+	InputStream io.Reader
 	// AttachOutput is whether to attach to STDOUT
 	// If false, stdout will not be attached
 	AttachOutput bool
