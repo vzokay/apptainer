sylabs/singularity

oci: switch to runc for oci commands
Replace invocations of SingularityCE's OCI runtime engine with wrapped
invocations of runc.

cmd/internal/cli/oci_linux.go
@@ -28,28 +28,6 @@ var ociBundleFlag = cmdline.Flag{
 	EnvKeys:      []string{"BUNDLE"},
 }
 
-// -s|--sync-socket
-var ociSyncSocketFlag = cmdline.Flag{
-	ID:           "ociSyncSocketFlag",
-	Value:        &ociArgs.SyncSocketPath,
-	DefaultValue: "",
-	Name:         "sync-socket",
-	ShortHand:    "s",
-	Usage:        "specify the path to unix socket for state synchronization",
-	Tag:          "<path>",
-	EnvKeys:      []string{"SYNC_SOCKET"},
-}
-
-// --empty-process
-var ociCreateEmptyProcessFlag = cmdline.Flag{
-	ID:           "ociCreateEmptyProcessFlag",
-	Value:        &ociArgs.EmptyProcess,
-	DefaultValue: false,
-	Name:         "empty-process",
-	Usage:        "run container without executing container process (eg: for POD container)",
-	EnvKeys:      []string{"EMPTY_PROCESS"},
-}
-
 // -l|--log-path
 var ociLogPathFlag = cmdline.Flag{
 	ID:           "ociLogPathFlag",
@@ -107,16 +85,6 @@ var ociKillForceFlag = cmdline.Flag{
 	EnvKeys:      []string{"FORCE"},
 }
 
-// -t|--timeout
-var ociKillTimeoutFlag = cmdline.Flag{
-	ID:           "ociKillTimeoutFlag",
-	Value:        &ociArgs.KillTimeout,
-	DefaultValue: uint32(0),
-	Name:         "timeout",
-	ShortHand:    "t",
-	Usage:        "timeout in second before killing container",
-}
-
 // -f|--from-file
 var ociUpdateFromFileFlag = cmdline.Flag{
 	ID:           "ociUpdateFromFileFlag",
@@ -149,16 +117,12 @@ func init() {
 		createRunCmd := cmdManager.GetCmdGroup("create_run")
 
 		cmdManager.RegisterFlagForCmd(&ociBundleFlag, createRunCmd...)
-		cmdManager.RegisterFlagForCmd(&ociSyncSocketFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociLogPathFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociLogFormatFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociPidFileFlag, createRunCmd...)
-		cmdManager.RegisterFlagForCmd(&ociCreateEmptyProcessFlag, OciCreateCmd)
 		cmdManager.RegisterFlagForCmd(&ociKillForceFlag, OciKillCmd)
 		cmdManager.RegisterFlagForCmd(&ociKillSignalFlag, OciKillCmd)
-		cmdManager.RegisterFlagForCmd(&ociKillTimeoutFlag, OciKillCmd)
 		cmdManager.RegisterFlagForCmd(&ociUpdateFromFileFlag, OciUpdateCmd)
-		cmdManager.RegisterFlagForCmd(&ociSyncSocketFlag, OciStateCmd)
 	})
 }
 
@@ -232,7 +196,6 @@ var OciKillCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		timeout := int(ociArgs.KillTimeout)
 		killSignal := ""
 		if len(args) > 1 && args[1] != "" {
 			killSignal = args[1]
@@ -242,7 +205,7 @@ var OciKillCmd = &cobra.Command{
 		if ociArgs.ForceKill {
 			killSignal = "SIGKILL"
 		}
-		if err := singularity.OciKill(args[0], killSignal, timeout); err != nil {
+		if err := singularity.OciKill(args[0], killSignal); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},
@@ -322,7 +285,7 @@ var OciPauseCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := singularity.OciPauseResume(args[0], true); err != nil {
+		if err := singularity.OciPause(args[0]); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},
@@ -338,7 +301,7 @@ var OciResumeCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := singularity.OciPauseResume(args[0], false); err != nil {
+		if err := singularity.OciResume(args[0]); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},

internal/app/singularity/oci_create_linux.go
@@ -6,27 +6,16 @@
 package singularity
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path/filepath"
+	"syscall"
 
-	"github.com/sylabs/singularity/internal/pkg/runtime/engine/config/oci/generate"
-	"github.com/sylabs/singularity/internal/pkg/runtime/engine/oci"
-	"github.com/sylabs/singularity/internal/pkg/util/starter"
-	"github.com/sylabs/singularity/pkg/runtime/engine/config"
+	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciCreate creates a container from an OCI bundle
 func OciCreate(containerID string, args *OciArgs) error {
-	_, err := getState(containerID)
-	if err == nil {
-		return fmt.Errorf("%s already exists", containerID)
-	}
-
-	os.Clearenv()
-
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
 		return fmt.Errorf("failed to determine bundle absolute path: %s", err)
@@ -36,46 +25,20 @@ func OciCreate(containerID string, args *OciArgs) error {
 		return fmt.Errorf("failed to change directory to %s: %s", absBundle, err)
 	}
 
-	engineConfig := oci.NewConfig()
-	generator := generate.New(&engineConfig.OciConfig.Spec)
-	engineConfig.SetBundlePath(absBundle)
-	engineConfig.SetLogPath(args.LogPath)
-	engineConfig.SetLogFormat(args.LogFormat)
-	engineConfig.SetPidFile(args.PidFile)
+	cmdArgs := []string{
+		"--root=" + OciStateDir,
+		"create",
+		"-b", absBundle,
+	}
+	if args.PidFile != "" {
+		cmdArgs = append(cmdArgs, "--pid-file="+args.PidFile)
+	}
+	cmdArgs = append(cmdArgs, containerID)
 
-	// load config.json from bundle path
-	configJSON := filepath.Join(absBundle, "config.json")
-	fb, err := os.Open(configJSON)
-	if err != nil {
-		return fmt.Errorf("oci specification file %q is missing or cannot be read", configJSON)
+	sylog.Debugf("Calling runc with args %v", cmdArgs)
+	if err := syscall.Exec(runc, cmdArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	data, err := ioutil.ReadAll(fb)
-	if err != nil {
-		return fmt.Errorf("failed to read OCI specification file %s: %s", configJSON, err)
-	}
-
-	fb.Close()
-
-	if err := json.Unmarshal(data, generator.Config); err != nil {
-		return fmt.Errorf("failed to parse OCI specification file %s: %s", configJSON, err)
-	}
-
-	engineConfig.EmptyProcess = args.EmptyProcess
-	engineConfig.SyncSocket = args.SyncSocketPath
-
-	commonConfig := &config.Common{
-		ContainerID:  containerID,
-		EngineName:   oci.Name,
-		EngineConfig: engineConfig,
-	}
-
-	procName := fmt.Sprintf("Singularity OCI %s", containerID)
-	return starter.Run(
-		procName,
-		commonConfig,
-		starter.WithStdin(os.Stdin),
-		starter.WithStderr(os.Stderr),
-		starter.WithStdout(os.Stdout),
-	)
+	return nil
 }

internal/app/singularity/oci_exec_linux.go
@@ -7,35 +7,24 @@ package singularity
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"syscall"
 
-	"github.com/sylabs/singularity/internal/pkg/runtime/engine/oci"
-	"github.com/sylabs/singularity/internal/pkg/util/starter"
-	"github.com/sylabs/singularity/pkg/ociruntime"
+	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciExec executes a command in a container
 func OciExec(containerID string, cmdArgs []string) error {
-	commonConfig, err := getCommonConfig(containerID)
-	if err != nil {
-		return fmt.Errorf("%s doesn't exist", containerID)
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"exec",
+		containerID,
+	}
+	runcArgs = append(runcArgs, cmdArgs...)
+
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	engineConfig := commonConfig.EngineConfig.(*oci.EngineConfig)
-
-	switch engineConfig.GetState().Status {
-	case ociruntime.Running, ociruntime.Paused:
-	default:
-		args := strings.Join(cmdArgs, " ")
-		return fmt.Errorf("cannot execute command %q, container '%s' is not running", args, containerID)
-	}
-
-	engineConfig.Exec = true
-	engineConfig.OciConfig.SetProcessArgs(cmdArgs)
-
-	os.Clearenv()
-
-	procName := fmt.Sprintf("Singularity OCI %s", containerID)
-	return starter.Exec(procName, commonConfig)
+	return nil
 }

internal/app/singularity/oci_pause_linux.go
@@ -6,68 +6,39 @@
 package singularity
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"syscall"
 
-	"github.com/sylabs/singularity/pkg/ociruntime"
-	"github.com/sylabs/singularity/pkg/util/unix"
+	"github.com/sylabs/singularity/pkg/sylog"
 )
 
-// OciPauseResume pauses/resumes processes in a container
-func OciPauseResume(containerID string, pause bool) error {
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+// OciPause pauses processes in a container
+func OciPause(containerID string) error {
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"pause",
+		containerID,
 	}
 
-	if state.ControlSocket == "" {
-		return fmt.Errorf("can't find control socket")
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	if pause && state.Status != ociruntime.Running {
-		return fmt.Errorf("container %s is not running", containerID)
-	} else if !pause && state.Status != ociruntime.Paused {
-		return fmt.Errorf("container %s is not paused", containerID)
+	return nil
+}
+
+// OciResume pauses processes in a container
+func OciResume(containerID string) error {
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"resume",
+		containerID,
 	}
 
-	ctrl := &ociruntime.Control{}
-	if pause {
-		ctrl.Pause = true
-	} else {
-		ctrl.Resume = true
-	}
-
-	c, err := unix.Dial(state.ControlSocket)
-	if err != nil {
-		return fmt.Errorf("failed to connect to control socket")
-	}
-	defer c.Close()
-
-	enc := json.NewEncoder(c)
-	if enc == nil {
-		return fmt.Errorf("cannot instantiate new JSON encoder")
-	}
-
-	if err := enc.Encode(ctrl); err != nil {
-		return err
-	}
-
-	// wait runtime close socket connection for ACK
-	d := make([]byte, 1)
-	if _, err := c.Read(d); err != io.EOF {
-		return err
-	}
-
-	// check status
-	state, err = getState(containerID)
-	if err != nil {
-		return err
-	}
-	if pause && state.Status != ociruntime.Paused {
-		return fmt.Errorf("bad status %s returned instead of paused", state.Status)
-	} else if !pause && state.Status != ociruntime.Running {
-		return fmt.Errorf("bad status %s returned instead of running", state.Status)
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
 	return nil

internal/app/singularity/oci_start_linux.go
@@ -6,51 +6,23 @@
 package singularity
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"syscall"
 
-	"github.com/sylabs/singularity/pkg/ociruntime"
-	"github.com/sylabs/singularity/pkg/util/unix"
+	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciStart starts a previously create container
 func OciStart(containerID string) error {
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"start",
+		containerID,
 	}
 
-	if state.Status != ociruntime.Created {
-		return fmt.Errorf("cannot start '%s', the state of the container must be %s", containerID, ociruntime.Created)
-	}
-
-	if state.ControlSocket == "" {
-		return fmt.Errorf("can't find control socket")
-	}
-
-	ctrl := &ociruntime.Control{}
-	ctrl.StartContainer = true
-
-	c, err := unix.Dial(state.ControlSocket)
-	if err != nil {
-		return fmt.Errorf("failed to connect to control socket")
-	}
-	defer c.Close()
-
-	enc := json.NewEncoder(c)
-	if enc == nil {
-		return fmt.Errorf("cannot instantiate new JSON encoder")
-	}
-
-	if err := enc.Encode(ctrl); err != nil {
-		return err
-	}
-
-	// wait runtime close socket connection for ACK
-	d := make([]byte, 1)
-	if _, err := c.Read(d); err != io.EOF {
-		return err
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
 	return nil

internal/app/singularity/oci_state_linux.go
@@ -6,32 +6,24 @@
 package singularity
 
 import (
-	"encoding/json"
 	"fmt"
+	"syscall"
 
-	"github.com/sylabs/singularity/pkg/util/unix"
+	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciState query container state
 func OciState(containerID string, args *OciArgs) error {
-	// query instance files and returns state
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"state",
+		containerID,
 	}
-	if args.SyncSocketPath != "" {
-		data, err := json.Marshal(state)
-		if err != nil {
-			return fmt.Errorf("failed to marshal state data: %s", err)
-		} else if err := unix.WriteSocket(args.SyncSocketPath, data); err != nil {
-			return err
-		}
-	} else {
-		c, err := json.MarshalIndent(state, "", "\t")
-		if err != nil {
-			return err
-		}
-		fmt.Println(string(c))
+
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
+
 	return nil
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/oci_linux.go
@@ -32,28 +32,6 @@ var ociBundleFlag = cmdline.Flag{
 	EnvKeys:      []string{"BUNDLE"},
 }
 
-// -s|--sync-socket
-var ociSyncSocketFlag = cmdline.Flag{
-	ID:           "ociSyncSocketFlag",
-	Value:        &ociArgs.SyncSocketPath,
-	DefaultValue: "",
-	Name:         "sync-socket",
-	ShortHand:    "s",
-	Usage:        "specify the path to unix socket for state synchronization",
-	Tag:          "<path>",
-	EnvKeys:      []string{"SYNC_SOCKET"},
-}
-
-// --empty-process
-var ociCreateEmptyProcessFlag = cmdline.Flag{
-	ID:           "ociCreateEmptyProcessFlag",
-	Value:        &ociArgs.EmptyProcess,
-	DefaultValue: false,
-	Name:         "empty-process",
-	Usage:        "run container without executing container process (eg: for POD container)",
-	EnvKeys:      []string{"EMPTY_PROCESS"},
-}
-
 // -l|--log-path
 var ociLogPathFlag = cmdline.Flag{
 	ID:           "ociLogPathFlag",
@@ -111,16 +89,6 @@ var ociKillForceFlag = cmdline.Flag{
 	EnvKeys:      []string{"FORCE"},
 }
 
-// -t|--timeout
-var ociKillTimeoutFlag = cmdline.Flag{
-	ID:           "ociKillTimeoutFlag",
-	Value:        &ociArgs.KillTimeout,
-	DefaultValue: uint32(0),
-	Name:         "timeout",
-	ShortHand:    "t",
-	Usage:        "timeout in second before killing container",
-}
-
 // -f|--from-file
 var ociUpdateFromFileFlag = cmdline.Flag{
 	ID:           "ociUpdateFromFileFlag",
@@ -153,16 +121,12 @@ func init() {
 		createRunCmd := cmdManager.GetCmdGroup("create_run")
 
 		cmdManager.RegisterFlagForCmd(&ociBundleFlag, createRunCmd...)
-		cmdManager.RegisterFlagForCmd(&ociSyncSocketFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociLogPathFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociLogFormatFlag, createRunCmd...)
 		cmdManager.RegisterFlagForCmd(&ociPidFileFlag, createRunCmd...)
-		cmdManager.RegisterFlagForCmd(&ociCreateEmptyProcessFlag, OciCreateCmd)
 		cmdManager.RegisterFlagForCmd(&ociKillForceFlag, OciKillCmd)
 		cmdManager.RegisterFlagForCmd(&ociKillSignalFlag, OciKillCmd)
-		cmdManager.RegisterFlagForCmd(&ociKillTimeoutFlag, OciKillCmd)
 		cmdManager.RegisterFlagForCmd(&ociUpdateFromFileFlag, OciUpdateCmd)
-		cmdManager.RegisterFlagForCmd(&ociSyncSocketFlag, OciStateCmd)
 	})
 }
 
@@ -236,7 +200,6 @@ var OciKillCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		timeout := int(ociArgs.KillTimeout)
 		killSignal := ""
 		if len(args) > 1 && args[1] != "" {
 			killSignal = args[1]
@@ -246,7 +209,7 @@ var OciKillCmd = &cobra.Command{
 		if ociArgs.ForceKill {
 			killSignal = "SIGKILL"
 		}
-		if err := apptainer.OciKill(args[0], killSignal, timeout); err != nil {
+		if err := apptainer.OciKill(args[0], killSignal); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},
@@ -326,7 +289,7 @@ var OciPauseCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := apptainer.OciPauseResume(args[0], true); err != nil {
+		if err := apptainer.OciPause(args[0]); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},
@@ -342,7 +305,7 @@ var OciResumeCmd = &cobra.Command{
 	DisableFlagsInUseLine: true,
 	PreRun:                CheckRoot,
 	Run: func(cmd *cobra.Command, args []string) {
-		if err := apptainer.OciPauseResume(args[0], false); err != nil {
+		if err := apptainer.OciResume(args[0]); err != nil {
 			sylog.Fatalf("%s", err)
 		}
 	},

internal/app/apptainer/oci_create_linux.go
@@ -10,27 +10,16 @@
 package apptainer
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
+	"syscall"
 
-	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/config/oci/generate"
-	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/oci"
-	"github.com/apptainer/apptainer/internal/pkg/util/starter"
-	"github.com/apptainer/apptainer/pkg/runtime/engine/config"
+	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciCreate creates a container from an OCI bundle
 func OciCreate(containerID string, args *OciArgs) error {
-	_, err := getState(containerID)
-	if err == nil {
-		return fmt.Errorf("%s already exists", containerID)
-	}
-
-	os.Clearenv()
-
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
 		return fmt.Errorf("failed to determine bundle absolute path: %s", err)
@@ -40,46 +29,20 @@ func OciCreate(containerID string, args *OciArgs) error {
 		return fmt.Errorf("failed to change directory to %s: %s", absBundle, err)
 	}
 
-	engineConfig := oci.NewConfig()
-	generator := generate.New(&engineConfig.OciConfig.Spec)
-	engineConfig.SetBundlePath(absBundle)
-	engineConfig.SetLogPath(args.LogPath)
-	engineConfig.SetLogFormat(args.LogFormat)
-	engineConfig.SetPidFile(args.PidFile)
+	cmdArgs := []string{
+		"--root=" + OciStateDir,
+		"create",
+		"-b", absBundle,
+	}
+	if args.PidFile != "" {
+		cmdArgs = append(cmdArgs, "--pid-file="+args.PidFile)
+	}
+	cmdArgs = append(cmdArgs, containerID)
 
-	// load config.json from bundle path
-	configJSON := filepath.Join(absBundle, "config.json")
-	fb, err := os.Open(configJSON)
-	if err != nil {
-		return fmt.Errorf("oci specification file %q is missing or cannot be read", configJSON)
+	sylog.Debugf("Calling runc with args %v", cmdArgs)
+	if err := syscall.Exec(runc, cmdArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	data, err := io.ReadAll(fb)
-	if err != nil {
-		return fmt.Errorf("failed to read OCI specification file %s: %s", configJSON, err)
-	}
-
-	fb.Close()
-
-	if err := json.Unmarshal(data, generator.Config); err != nil {
-		return fmt.Errorf("failed to parse OCI specification file %s: %s", configJSON, err)
-	}
-
-	engineConfig.EmptyProcess = args.EmptyProcess
-	engineConfig.SyncSocket = args.SyncSocketPath
-
-	commonConfig := &config.Common{
-		ContainerID:  containerID,
-		EngineName:   oci.Name,
-		EngineConfig: engineConfig,
-	}
-
-	procName := fmt.Sprintf("Apptainer OCI %s", containerID)
-	return starter.Run(
-		procName,
-		commonConfig,
-		starter.WithStdin(os.Stdin),
-		starter.WithStderr(os.Stderr),
-		starter.WithStdout(os.Stdout),
-	)
+	return nil
 }

internal/app/apptainer/oci_exec_linux.go
@@ -11,35 +11,24 @@ package apptainer
 
 import (
 	"fmt"
-	"os"
-	"strings"
+	"syscall"
 
-	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/oci"
-	"github.com/apptainer/apptainer/internal/pkg/util/starter"
-	"github.com/apptainer/apptainer/pkg/ociruntime"
+	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciExec executes a command in a container
 func OciExec(containerID string, cmdArgs []string) error {
-	commonConfig, err := getCommonConfig(containerID)
-	if err != nil {
-		return fmt.Errorf("%s doesn't exist", containerID)
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"exec",
+		containerID,
+	}
+	runcArgs = append(runcArgs, cmdArgs...)
+
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	engineConfig := commonConfig.EngineConfig.(*oci.EngineConfig)
-
-	switch engineConfig.GetState().Status {
-	case ociruntime.Running, ociruntime.Paused:
-	default:
-		args := strings.Join(cmdArgs, " ")
-		return fmt.Errorf("cannot execute command %q, container '%s' is not running", args, containerID)
-	}
-
-	engineConfig.Exec = true
-	engineConfig.OciConfig.SetProcessArgs(cmdArgs)
-
-	os.Clearenv()
-
-	procName := fmt.Sprintf("Apptainer OCI %s", containerID)
-	return starter.Exec(procName, commonConfig)
+	return nil
 }

internal/app/apptainer/oci_pause_linux.go
@@ -10,68 +10,39 @@
 package apptainer
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"syscall"
 
-	"github.com/apptainer/apptainer/pkg/ociruntime"
-	"github.com/apptainer/apptainer/pkg/util/unix"
+	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
-// OciPauseResume pauses/resumes processes in a container
-func OciPauseResume(containerID string, pause bool) error {
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+// OciPause pauses processes in a container
+func OciPause(containerID string) error {
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"pause",
+		containerID,
 	}
 
-	if state.ControlSocket == "" {
-		return fmt.Errorf("can't find control socket")
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
-	if pause && state.Status != ociruntime.Running {
-		return fmt.Errorf("container %s is not running", containerID)
-	} else if !pause && state.Status != ociruntime.Paused {
-		return fmt.Errorf("container %s is not paused", containerID)
+	return nil
+}
+
+// OciResume pauses processes in a container
+func OciResume(containerID string) error {
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"resume",
+		containerID,
 	}
 
-	ctrl := &ociruntime.Control{}
-	if pause {
-		ctrl.Pause = true
-	} else {
-		ctrl.Resume = true
-	}
-
-	c, err := unix.Dial(state.ControlSocket)
-	if err != nil {
-		return fmt.Errorf("failed to connect to control socket")
-	}
-	defer c.Close()
-
-	enc := json.NewEncoder(c)
-	if enc == nil {
-		return fmt.Errorf("cannot instantiate new JSON encoder")
-	}
-
-	if err := enc.Encode(ctrl); err != nil {
-		return err
-	}
-
-	// wait runtime close socket connection for ACK
-	d := make([]byte, 1)
-	if _, err := c.Read(d); err != io.EOF {
-		return err
-	}
-
-	// check status
-	state, err = getState(containerID)
-	if err != nil {
-		return err
-	}
-	if pause && state.Status != ociruntime.Paused {
-		return fmt.Errorf("bad status %s returned instead of paused", state.Status)
-	} else if !pause && state.Status != ociruntime.Running {
-		return fmt.Errorf("bad status %s returned instead of running", state.Status)
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
 	return nil

internal/app/apptainer/oci_start_linux.go
@@ -10,51 +10,23 @@
 package apptainer
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
+	"syscall"
 
-	"github.com/apptainer/apptainer/pkg/ociruntime"
-	"github.com/apptainer/apptainer/pkg/util/unix"
+	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciStart starts a previously create container
 func OciStart(containerID string) error {
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"start",
+		containerID,
 	}
 
-	if state.Status != ociruntime.Created {
-		return fmt.Errorf("cannot start '%s', the state of the container must be %s", containerID, ociruntime.Created)
-	}
-
-	if state.ControlSocket == "" {
-		return fmt.Errorf("can't find control socket")
-	}
-
-	ctrl := &ociruntime.Control{}
-	ctrl.StartContainer = true
-
-	c, err := unix.Dial(state.ControlSocket)
-	if err != nil {
-		return fmt.Errorf("failed to connect to control socket")
-	}
-	defer c.Close()
-
-	enc := json.NewEncoder(c)
-	if enc == nil {
-		return fmt.Errorf("cannot instantiate new JSON encoder")
-	}
-
-	if err := enc.Encode(ctrl); err != nil {
-		return err
-	}
-
-	// wait runtime close socket connection for ACK
-	d := make([]byte, 1)
-	if _, err := c.Read(d); err != io.EOF {
-		return err
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
 
 	return nil

internal/app/apptainer/oci_state_linux.go
@@ -10,32 +10,24 @@
 package apptainer
 
 import (
-	"encoding/json"
 	"fmt"
+	"syscall"
 
-	"github.com/apptainer/apptainer/pkg/util/unix"
+	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciState query container state
 func OciState(containerID string, args *OciArgs) error {
-	// query instance files and returns state
-	state, err := getState(containerID)
-	if err != nil {
-		return err
+	runcArgs := []string{
+		"--root=" + OciStateDir,
+		"state",
+		containerID,
 	}
-	if args.SyncSocketPath != "" {
-		data, err := json.Marshal(state)
-		if err != nil {
-			return fmt.Errorf("failed to marshal state data: %s", err)
-		} else if err := unix.WriteSocket(args.SyncSocketPath, data); err != nil {
-			return err
-		}
-	} else {
-		c, err := json.MarshalIndent(state, "", "\t")
-		if err != nil {
-			return err
-		}
-		fmt.Println(string(c))
+
+	sylog.Debugf("Calling runc with args %v", runcArgs)
+	if err := syscall.Exec(runc, runcArgs, []string{}); err != nil {
+		return fmt.Errorf("while calling runc: %w", err)
 	}
+
 	return nil
 }