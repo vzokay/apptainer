0e059fefe5e2ca692624a3941306cc3298b9e2b8
David Trudgian <david.trudgian@sylabs.io>
2022-12-02 14:12:58 +0000

    e2e: port relevant --bind/mount action tests for --oci
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- e2e/actions/actions.go
+++ e2e/actions/actions.go
@@ -2916,5 +2916,6 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"ociExec":    c.actionOciExec,    // apptainer exec --oci
 		"ociShell":   c.actionOciShell,   // apptainer shell --oci
 		"ociNetwork": c.actionOciNetwork, // apptainer exec --oci --net
+		"ociBinds":   c.actionOciBinds,   // apptainer exec --oci --bind / --mount
 	}
 }
