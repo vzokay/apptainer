9521bcd03b39671f449d4cfd036152f13fb5baef
David Trudgian <david.trudgian@sylabs.io>
2022-12-06 11:20:29 +0000

    e2e: add SINGULARITYENV_ tests for --oci mode
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- e2e/env/env.go
+++ e2e/env/env.go
@@ -647,6 +647,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// --oci mode
 		//
-		"oci environment option": c.ociEnvOption,
+		"oci environment apptainerenv": c.ociApptainerEnv,
+		"oci environment option":       c.ociEnvOption,
 	}
 }
