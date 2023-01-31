sylabs/singularity

feat: add '--key' option for sign/verify
Add basic sign/verify using PEM-encoded SSL keys. Add E2E tests for the
same.

cmd/internal/cli/sign.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -6,8 +6,11 @@
 package cli
 
 import (
+	"crypto"
 	"fmt"
 
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
+	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
 	"github.com/sylabs/singularity/internal/app/singularity"
@@ -17,8 +20,9 @@ import (
 )
 
 var (
-	privKey int // -k encryption key (index from 'key list --secret') specification
-	signAll bool
+	priKeyPath string
+	priKeyIdx  int
+	signAll    bool
 )
 
 // -g|--group-id
@@ -61,14 +65,23 @@ var signSifDescIDFlag = cmdline.Flag{
 	Deprecated:   "use '--sif-id'",
 }
 
+// --key
+var signPrivateKeyFlag = cmdline.Flag{
+	ID:           "privateKeyFlag",
+	Value:        &priKeyPath,
+	DefaultValue: "",
+	Name:         "key",
+	Usage:        "path to the private key file",
+}
+
 // -k|--keyidx
 var signKeyIdxFlag = cmdline.Flag{
 	ID:           "signKeyIdxFlag",
-	Value:        &privKey,
+	Value:        &priKeyIdx,
 	DefaultValue: 0,
 	Name:         "keyidx",
 	ShortHand:    "k",
-	Usage:        "private key to use (index from 'key list --secret')",
+	Usage:        "PGP private key to use (index from 'key list --secret')",
 }
 
 // -a|--all (deprecated)
@@ -90,6 +103,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&signOldSifGroupIDFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signSifDescSifIDFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signSifDescIDFlag, SignCmd)
+		cmdManager.RegisterFlagForCmd(&signPrivateKeyFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signKeyIdxFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signAllFlag, SignCmd)
 	})
@@ -114,15 +128,26 @@ var SignCmd = &cobra.Command{
 func doSignCmd(cmd *cobra.Command, cpath string) {
 	var opts []singularity.SignOpt
 
-	// Set entity selector option, and ensure the entity is decrypted.
-	var f sypgp.EntitySelector
-	if cmd.Flag(signKeyIdxFlag.Name).Changed {
-		f = selectEntityAtIndex(privKey)
-	} else {
-		f = selectEntityInteractive()
+	// Set key material.
+	switch {
+	case cmd.Flag(signPrivateKeyFlag.Name).Changed:
+		s, err := signature.LoadSignerFromPEMFile(priKeyPath, crypto.SHA256, cryptoutils.GetPasswordFromStdIn)
+		if err != nil {
+			sylog.Fatalf("Failed to load key material: %v", err)
+		}
+		opts = append(opts, singularity.OptSignWithSigner(s))
+
+	default:
+		// Set entity selector option, and ensure the entity is decrypted.
+		var f sypgp.EntitySelector
+		if cmd.Flag(signKeyIdxFlag.Name).Changed {
+			f = selectEntityAtIndex(priKeyIdx)
+		} else {
+			f = selectEntityInteractive()
+		}
+		f = decryptSelectedEntityInteractive(f)
+		opts = append(opts, singularity.OptSignEntitySelector(f))
 	}
-	f = decryptSelectedEntityInteractive(f)
-	opts = append(opts, singularity.OptSignEntitySelector(f))
 
 	// Set group option, if applicable.
 	if cmd.Flag(signSifGroupIDFlag.Name).Changed || cmd.Flag(signOldSifGroupIDFlag.Name).Changed {

cmd/internal/cli/verify.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2017-2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -7,9 +7,11 @@
 package cli
 
 import (
+	"crypto"
 	"fmt"
 	"os"
 
+	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
 	"github.com/sylabs/singularity/internal/app/singularity"
@@ -21,6 +23,7 @@ import (
 var (
 	sifGroupID   uint32 // -g groupid specification
 	sifDescID    uint32 // -i id specification
+	pubKeyPath   string // --key flag
 	localVerify  bool   // -l flag
 	jsonVerify   bool   // -j flag
 	verifyAll    bool
@@ -78,6 +81,15 @@ var verifySifDescIDFlag = cmdline.Flag{
 	Deprecated:   "use '--sif-id'",
 }
 
+// --key
+var verifyPublicKeyFlag = cmdline.Flag{
+	ID:           "publicKeyFlag",
+	Value:        &pubKeyPath,
+	DefaultValue: "",
+	Name:         "key",
+	Usage:        "path to the public key file",
+}
+
 // -l|--local
 var verifyLocalFlag = cmdline.Flag{
 	ID:           "verifyLocalFlag",
@@ -127,6 +139,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyOldSifGroupIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescSifIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescIDFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyAllFlag, VerifyCmd)
@@ -153,14 +166,25 @@ var VerifyCmd = &cobra.Command{
 func doVerifyCmd(cmd *cobra.Command, cpath string) {
 	var opts []singularity.VerifyOpt
 
-	// Set keyserver option, if applicable.
-	if !localVerify {
-		co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
+	switch {
+	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
+		v, err := signature.LoadVerifierFromPEMFile(pubKeyPath, crypto.SHA256)
 		if err != nil {
-			sylog.Fatalf("Error while getting keyserver client config: %v", err)
+			sylog.Fatalf("Failed to load key material: %v", err)
 		}
+		opts = append(opts, singularity.OptVerifyWithVerifier(v))
 
-		opts = append(opts, singularity.OptVerifyUseKeyServer(co...))
+	default:
+		// Set keyserver option, if applicable.
+		if localVerify {
+			opts = append(opts, singularity.OptVerifyWithPGP())
+		} else {
+			co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
+			if err != nil {
+				sylog.Fatalf("Error while getting keyserver client config: %v", err)
+			}
+			opts = append(opts, singularity.OptVerifyWithPGP(co...))
+		}
 	}
 
 	// Set group option, if applicable.

e2e/sign/sign.go
@@ -196,6 +196,26 @@ func (c ctx) singularitySignKeyidxOption(t *testing.T) {
 	)
 }
 
+func (c ctx) singularitySignKeyOption(t *testing.T) {
+	imgPath, cleanup := c.prepareImage(t)
+	defer cleanup(t)
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("sign"),
+		e2e.WithArgs(
+			"--key",
+			filepath.Join("..", "test", "keys", "private.pem"),
+			imgPath,
+		),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Signature created and applied to "+imgPath),
+		),
+	)
+}
+
 func (c *ctx) generateKeypair(t *testing.T) {
 	keyGenInput := []e2e.SingularityConsoleOp{
 		e2e.ConsoleSendLine("e2e sign test key"),
@@ -247,6 +267,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("singularitySignIDOption", c.singularitySignIDOption)
 			t.Run("singularitySignGroupIDOption", c.singularitySignGroupIDOption)
 			t.Run("singularitySignKeyidxOption", c.singularitySignKeyidxOption)
+			t.Run("singularitySignKeyOption", c.singularitySignKeyOption)
 		},
 	}
 }

e2e/verify/verify.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -305,6 +305,25 @@ func (c ctx) checkURLOption(t *testing.T) {
 	)
 }
 
+func (c ctx) singularityVerifyKeyOption(t *testing.T) {
+	imagePath := filepath.Join("testdata", "images", "one-group-signed-dsse.sif")
+
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("verify"),
+		e2e.WithArgs(
+			"--key",
+			filepath.Join("..", "test", "keys", "public.pem"),
+			imagePath,
+		),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Container verified: "+imagePath),
+		),
+	)
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := ctx{
@@ -326,6 +345,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("singularityVerifyGroupIdOption", c.checkGroupidOption)
 			t.Run("singularityVerifyIDOption", c.checkIDOption)
 			t.Run("singularityVerifyURLOption", c.checkURLOption)
+			t.Run("singularityVerifyKeyOption", c.singularityVerifyKeyOption)
 		},
 	}
 }

internal/pkg/build/sources/verify_sif.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2020-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -16,11 +16,11 @@ import (
 // checkSIFFingerprint checks whether a bootstrap SIF image verifies, and was signed with a specified fingerprint
 func checkSIFFingerprint(ctx context.Context, imagePath string, fingerprints []string, co ...scskeyclient.Option) error {
 	sylog.Infof("Checking bootstrap image verifies with fingerprint(s): %v", fingerprints)
-	return singularity.VerifyFingerprints(ctx, imagePath, fingerprints, singularity.OptVerifyUseKeyServer(co...))
+	return singularity.VerifyFingerprints(ctx, imagePath, fingerprints, singularity.OptVerifyWithPGP(co...))
 }
 
 // verifySIF checks whether a bootstrap SIF image verifies
 func verifySIF(ctx context.Context, imagePath string, co ...scskeyclient.Option) error {
 	sylog.Infof("Verifying bootstrap image %s", imagePath)
-	return singularity.Verify(ctx, imagePath, singularity.OptVerifyUseKeyServer(co...))
+	return singularity.Verify(ctx, imagePath, singularity.OptVerifyWithPGP(co...))
 }

internal/pkg/client/library/pull.go
@@ -140,7 +140,7 @@ func PullToFile(ctx context.Context, imgCache *cache.Handle, pullTo string, pull
 		}
 	}
 
-	if err := singularity.Verify(ctx, pullTo, singularity.OptVerifyUseKeyServer(co...)); err != nil {
+	if err := singularity.Verify(ctx, pullTo, singularity.OptVerifyWithPGP(co...)); err != nil {
 		sylog.Warningf("%v", err)
 		return pullTo, ErrLibraryPullUnsigned
 	}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/sign.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2017-2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -10,6 +10,7 @@
 package cli
 
 import (
+	"crypto"
 	"fmt"
 
 	"github.com/apptainer/apptainer/docs"
@@ -17,12 +18,15 @@ import (
 	"github.com/apptainer/apptainer/pkg/cmdline"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	"github.com/apptainer/apptainer/pkg/sypgp"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
+	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 )
 
 var (
-	privKey int // -k encryption key (index from 'key list --secret') specification
-	signAll bool
+	priKeyPath string
+	priKeyIdx  int
+	signAll    bool
 )
 
 // -g|--group-id
@@ -65,14 +69,23 @@ var signSifDescIDFlag = cmdline.Flag{
 	Deprecated:   "use '--sif-id'",
 }
 
+// --key
+var signPrivateKeyFlag = cmdline.Flag{
+	ID:           "privateKeyFlag",
+	Value:        &priKeyPath,
+	DefaultValue: "",
+	Name:         "key",
+	Usage:        "path to the private key file",
+}
+
 // -k|--keyidx
 var signKeyIdxFlag = cmdline.Flag{
 	ID:           "signKeyIdxFlag",
-	Value:        &privKey,
+	Value:        &priKeyIdx,
 	DefaultValue: 0,
 	Name:         "keyidx",
 	ShortHand:    "k",
-	Usage:        "private key to use (index from 'key list --secret')",
+	Usage:        "PGP private key to use (index from 'key list --secret')",
 }
 
 // -a|--all (deprecated)
@@ -94,6 +107,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&signOldSifGroupIDFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signSifDescSifIDFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signSifDescIDFlag, SignCmd)
+		cmdManager.RegisterFlagForCmd(&signPrivateKeyFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signKeyIdxFlag, SignCmd)
 		cmdManager.RegisterFlagForCmd(&signAllFlag, SignCmd)
 	})
@@ -118,15 +132,26 @@ var SignCmd = &cobra.Command{
 func doSignCmd(cmd *cobra.Command, cpath string) {
 	var opts []apptainer.SignOpt
 
-	// Set entity selector option, and ensure the entity is decrypted.
-	var f sypgp.EntitySelector
-	if cmd.Flag(signKeyIdxFlag.Name).Changed {
-		f = selectEntityAtIndex(privKey)
-	} else {
-		f = selectEntityInteractive()
+	// Set key material.
+	switch {
+	case cmd.Flag(signPrivateKeyFlag.Name).Changed:
+		s, err := signature.LoadSignerFromPEMFile(priKeyPath, crypto.SHA256, cryptoutils.GetPasswordFromStdIn)
+		if err != nil {
+			sylog.Fatalf("Failed to load key material: %v", err)
+		}
+		opts = append(opts, apptainer.OptSignWithSigner(s))
+
+	default:
+		// Set entity selector option, and ensure the entity is decrypted.
+		var f sypgp.EntitySelector
+		if cmd.Flag(signKeyIdxFlag.Name).Changed {
+			f = selectEntityAtIndex(priKeyIdx)
+		} else {
+			f = selectEntityInteractive()
+		}
+		f = decryptSelectedEntityInteractive(f)
+		opts = append(opts, apptainer.OptSignEntitySelector(f))
 	}
-	f = decryptSelectedEntityInteractive(f)
-	opts = append(opts, apptainer.OptSignEntitySelector(f))
 
 	// Set group option, if applicable.
 	if cmd.Flag(signSifGroupIDFlag.Name).Changed || cmd.Flag(signOldSifGroupIDFlag.Name).Changed {

cmd/internal/cli/verify.go
@@ -3,7 +3,7 @@
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2017-2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2017-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -11,6 +11,7 @@
 package cli
 
 import (
+	"crypto"
 	"fmt"
 	"os"
 
@@ -19,12 +20,14 @@ import (
 	"github.com/apptainer/apptainer/internal/pkg/remote/endpoint"
 	"github.com/apptainer/apptainer/pkg/cmdline"
 	"github.com/apptainer/apptainer/pkg/sylog"
+	"github.com/sigstore/sigstore/pkg/signature"
 	"github.com/spf13/cobra"
 )
 
 var (
 	sifGroupID   uint32 // -g groupid specification
 	sifDescID    uint32 // -i id specification
+	pubKeyPath   string // --key flag
 	localVerify  bool   // -l flag
 	jsonVerify   bool   // -j flag
 	verifyAll    bool
@@ -82,6 +85,15 @@ var verifySifDescIDFlag = cmdline.Flag{
 	Deprecated:   "use '--sif-id'",
 }
 
+// --key
+var verifyPublicKeyFlag = cmdline.Flag{
+	ID:           "publicKeyFlag",
+	Value:        &pubKeyPath,
+	DefaultValue: "",
+	Name:         "key",
+	Usage:        "path to the public key file",
+}
+
 // -l|--local
 var verifyLocalFlag = cmdline.Flag{
 	ID:           "verifyLocalFlag",
@@ -131,6 +143,7 @@ func init() {
 		cmdManager.RegisterFlagForCmd(&verifyOldSifGroupIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescSifIDFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifySifDescIDFlag, VerifyCmd)
+		cmdManager.RegisterFlagForCmd(&verifyPublicKeyFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyLocalFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyJSONFlag, VerifyCmd)
 		cmdManager.RegisterFlagForCmd(&verifyAllFlag, VerifyCmd)
@@ -157,14 +170,25 @@ var VerifyCmd = &cobra.Command{
 func doVerifyCmd(cmd *cobra.Command, cpath string) {
 	var opts []apptainer.VerifyOpt
 
-	// Set keyserver option, if applicable.
-	if !localVerify {
-		co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
+	switch {
+	case cmd.Flag(verifyPublicKeyFlag.Name).Changed:
+		v, err := signature.LoadVerifierFromPEMFile(pubKeyPath, crypto.SHA256)
 		if err != nil {
-			sylog.Fatalf("Error while getting keyserver client config: %v", err)
+			sylog.Fatalf("Failed to load key material: %v", err)
 		}
+		opts = append(opts, apptainer.OptVerifyWithVerifier(v))
 
-		opts = append(opts, apptainer.OptVerifyUseKeyServer(co...))
+	default:
+		// Set keyserver option, if applicable.
+		if localVerify {
+			opts = append(opts, apptainer.OptVerifyWithPGP())
+		} else {
+			co, err := getKeyserverClientOpts(keyServerURI, endpoint.KeyserverVerifyOp)
+			if err != nil {
+				sylog.Fatalf("Error while getting keyserver client config: %v", err)
+			}
+			opts = append(opts, apptainer.OptVerifyWithPGP(co...))
+		}
 	}
 
 	// Set group option, if applicable.

e2e/sign/sign.go
@@ -200,6 +200,26 @@ func (c ctx) apptainerSignKeyidxOption(t *testing.T) {
 	)
 }
 
+func (c ctx) apptainerSignKeyOption(t *testing.T) {
+	imgPath, cleanup := c.prepareImage(t)
+	defer cleanup(t)
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("sign"),
+		e2e.WithArgs(
+			"--key",
+			filepath.Join("..", "test", "keys", "private.pem"),
+			imgPath,
+		),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Signature created and applied to "+imgPath),
+		),
+	)
+}
+
 func (c *ctx) generateKeypair(t *testing.T) {
 	keyGenInput := []e2e.ApptainerConsoleOp{
 		e2e.ConsoleSendLine("e2e sign test key"),
@@ -251,6 +271,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("apptainerSignIDOption", c.apptainerSignIDOption)
 			t.Run("apptainerSignGroupIDOption", c.apptainerSignGroupIDOption)
 			t.Run("apptainerSignKeyidxOption", c.apptainerSignKeyidxOption)
+			t.Run("apptainerSignKeyOption", c.apptainerSignKeyOption)
 		},
 	}
 }

e2e/verify/verify.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2019, Sylabs Inc. All rights reserved.
+// Copyright (c) 2019-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -310,6 +310,25 @@ func (c ctx) checkURLOption(t *testing.T) {
 	)
 }
 
+func (c ctx) apptainerVerifyKeyOption(t *testing.T) {
+	imagePath := filepath.Join("testdata", "images", "one-group-signed-dsse.sif")
+
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("verify"),
+		e2e.WithArgs(
+			"--key",
+			filepath.Join("..", "test", "keys", "public.pem"),
+			imagePath,
+		),
+		e2e.ExpectExit(
+			0,
+			e2e.ExpectOutput(e2e.ContainMatch, "Container verified: "+imagePath),
+		),
+	)
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := ctx{
@@ -331,6 +350,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 			t.Run("apptainerVerifyGroupIdOption", c.checkGroupidOption)
 			t.Run("apptainerVerifyIDOption", c.checkIDOption)
 			t.Run("apptainerVerifyURLOption", c.checkURLOption)
+			t.Run("apptainerVerifyKeyOption", c.apptainerVerifyKeyOption)
 		},
 	}
 }

internal/pkg/build/sources/verify_sif.go
@@ -2,7 +2,7 @@
 //   Apptainer a Series of LF Projects LLC.
 //   For website terms of use, trademark policy, privacy policy and other
 //   project policies see https://lfprojects.org/policies
-// Copyright (c) 2020, Sylabs Inc. All rights reserved.
+// Copyright (c) 2020-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -20,11 +20,11 @@ import (
 // checkSIFFingerprint checks whether a bootstrap SIF image verifies, and was signed with a specified fingerprint
 func checkSIFFingerprint(ctx context.Context, imagePath string, fingerprints []string, co ...keyClient.Option) error {
 	sylog.Infof("Checking bootstrap image verifies with fingerprint(s): %v", fingerprints)
-	return apptainer.VerifyFingerprints(ctx, imagePath, fingerprints, apptainer.OptVerifyUseKeyServer(co...))
+	return apptainer.VerifyFingerprints(ctx, imagePath, fingerprints, apptainer.OptVerifyWithPGP(co...))
 }
 
 // verifySIF checks whether a bootstrap SIF image verifies
 func verifySIF(ctx context.Context, imagePath string, co ...keyClient.Option) error {
 	sylog.Infof("Verifying bootstrap image %s", imagePath)
-	return apptainer.Verify(ctx, imagePath, apptainer.OptVerifyUseKeyServer(co...))
+	return apptainer.Verify(ctx, imagePath, apptainer.OptVerifyWithPGP(co...))
 }

internal/pkg/client/library/pull.go
@@ -143,7 +143,7 @@ func PullToFile(ctx context.Context, imgCache *cache.Handle, pullTo string, pull
 		}
 	}
 
-	if err := apptainer.Verify(ctx, pullTo, apptainer.OptVerifyUseKeyServer(co...)); err != nil {
+	if err := apptainer.Verify(ctx, pullTo, apptainer.OptVerifyWithPGP(co...)); err != nil {
 		sylog.Warningf("%v", err)
 		return pullTo, ErrLibraryPullUnsigned
 	}