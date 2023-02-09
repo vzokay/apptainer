sylabs/singularity

oci: use FindBin for conmon, runc

internal/app/singularity/oci_create_linux.go
@@ -23,12 +23,21 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/sylabs/singularity/internal/pkg/buildcfg"
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"golang.org/x/sys/unix"
 )
 
 // OciCreate creates a container from an OCI bundle
 func OciCreate(containerID string, args *OciArgs) error {
+	conmon, err := bin.FindBin("conmon")
+	if err != nil {
+		return err
+	}
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	// chdir to bundle and lock it, so another oci create cannot use the same bundle
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
@@ -111,7 +120,7 @@ func OciCreate(containerID string, args *OciArgs) error {
 	sylog.Debugf("Starting conmon with args %v", cmdArgs)
 	if err := cmd.Start(); err != nil {
 		if err2 := releaseBundle(absBundle); err2 != nil {
-			sylog.Errorf("while releasing bundle: %w", containerID)
+			sylog.Errorf("while releasing bundle: %v", err)
 		}
 		return fmt.Errorf("while starting conmon: %w", err)
 	}
@@ -125,7 +134,7 @@ func OciCreate(containerID string, args *OciArgs) error {
 	err = cmd.Wait()
 	if err != nil {
 		if err2 := releaseBundle(absBundle); err2 != nil {
-			sylog.Errorf("while releasing bundle: %w", containerID)
+			sylog.Errorf("while releasing bundle: %v", err)
 		}
 		return fmt.Errorf("while starting conmon: %w", err)
 	}

internal/app/singularity/oci_delete_linux.go
@@ -12,11 +12,16 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciDelete deletes container resources
 func OciDelete(ctx context.Context, containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"delete",
@@ -28,7 +33,7 @@ func OciDelete(ctx context.Context, containerID string) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdout
 	sylog.Debugf("Calling runc with args %v", runcArgs)
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("while calling runc delete: %w", err)
 	}

internal/app/singularity/oci_kill_linux.go
@@ -9,11 +9,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciKill kills container process
 func OciKill(containerID string, killSignal string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"kill",

internal/app/singularity/oci_pause_linux.go
@@ -9,11 +9,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciPause pauses processes in a container
 func OciPause(containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"pause",
@@ -30,6 +35,10 @@ func OciPause(containerID string) error {
 
 // OciResume pauses processes in a container
 func OciResume(containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"resume",

internal/app/singularity/oci_run_linux.go
@@ -12,11 +12,16 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciRun runs a container (equivalent to create/start/delete)
 func OciRun(ctx context.Context, containerID string, args *OciArgs) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
 		return fmt.Errorf("failed to determine bundle absolute path: %s", err)

internal/app/singularity/oci_update_linux.go
@@ -9,11 +9,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/sylabs/singularity/internal/pkg/util/bin"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
 
 // OciUpdate updates container cgroups resources
 func OciUpdate(containerID string, args *OciArgs) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"update",

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/app/apptainer/oci_create_linux.go
@@ -25,6 +25,7 @@ import (
 	"time"
 
 	"github.com/apptainer/apptainer/internal/pkg/buildcfg"
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 	"github.com/google/uuid"
 	"golang.org/x/sys/unix"
@@ -32,6 +33,14 @@ import (
 
 // OciCreate creates a container from an OCI bundle
 func OciCreate(containerID string, args *OciArgs) error {
+	conmon, err := bin.FindBin("conmon")
+	if err != nil {
+		return err
+	}
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	// chdir to bundle and lock it, so another oci create cannot use the same bundle
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
@@ -114,7 +123,7 @@ func OciCreate(containerID string, args *OciArgs) error {
 	sylog.Debugf("Starting conmon with args %v", cmdArgs)
 	if err := cmd.Start(); err != nil {
 		if err2 := releaseBundle(absBundle); err2 != nil {
-			sylog.Errorf("while releasing bundle: %w", containerID)
+			sylog.Errorf("while releasing bundle: %v", err)
 		}
 		return fmt.Errorf("while starting conmon: %w", err)
 	}
@@ -128,7 +137,7 @@ func OciCreate(containerID string, args *OciArgs) error {
 	err = cmd.Wait()
 	if err != nil {
 		if err2 := releaseBundle(absBundle); err2 != nil {
-			sylog.Errorf("while releasing bundle: %w", containerID)
+			sylog.Errorf("while releasing bundle: %v", err)
 		}
 		return fmt.Errorf("while starting conmon: %w", err)
 	}

internal/app/apptainer/oci_delete_linux.go
@@ -16,11 +16,16 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciDelete deletes container resources
 func OciDelete(ctx context.Context, containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"delete",
@@ -32,7 +37,7 @@ func OciDelete(ctx context.Context, containerID string) error {
 	cmd.Stderr = os.Stderr
 	cmd.Stdin = os.Stdout
 	sylog.Debugf("Calling runc with args %v", runcArgs)
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("while calling runc delete: %w", err)
 	}

internal/app/apptainer/oci_kill_linux.go
@@ -13,11 +13,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciKill kills container process
 func OciKill(containerID string, killSignal string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"kill",

internal/app/apptainer/oci_pause_linux.go
@@ -13,11 +13,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciPause pauses processes in a container
 func OciPause(containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"pause",
@@ -34,6 +39,10 @@ func OciPause(containerID string) error {
 
 // OciResume pauses processes in a container
 func OciResume(containerID string) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"resume",

internal/app/apptainer/oci_run_linux.go
@@ -16,11 +16,16 @@ import (
 	"os/exec"
 	"path/filepath"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciRun runs a container (equivalent to create/start/delete)
 func OciRun(ctx context.Context, containerID string, args *OciArgs) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	absBundle, err := filepath.Abs(args.BundlePath)
 	if err != nil {
 		return fmt.Errorf("failed to determine bundle absolute path: %s", err)

internal/app/apptainer/oci_update_linux.go
@@ -13,11 +13,16 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/apptainer/apptainer/internal/pkg/util/bin"
 	"github.com/apptainer/apptainer/pkg/sylog"
 )
 
 // OciUpdate updates container cgroups resources
 func OciUpdate(containerID string, args *OciArgs) error {
+	runc, err := bin.FindBin("runc")
+	if err != nil {
+		return err
+	}
 	runcArgs := []string{
 		"--root", RuncStateDir,
 		"update",