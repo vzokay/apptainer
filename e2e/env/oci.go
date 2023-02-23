sylabs/singularity

e2e: add --oci --env tests

e2e/env/oci.go
@@ -1,0 +1,122 @@
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package singularityenv
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/sylabs/singularity/e2e/internal/e2e"
+)
+
+func (c ctx) ociEnvOption(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+	defaultImage := "oci-archive:" + c.env.OCIImagePath
+
+	tests := []struct {
+		name     string
+		image    string
+		envOpt   []string
+		hostEnv  []string
+		matchEnv string
+		matchVal string
+	}{
+		{
+			name:     "DefaultPath",
+			image:    defaultImage,
+			matchEnv: "PATH",
+			matchVal: defaultPath,
+		},
+		{
+			name:     "DefaultPathOverride",
+			image:    defaultImage,
+			envOpt:   []string{"PATH=/"},
+			matchEnv: "PATH",
+			matchVal: "/",
+		},
+		{
+			name:     "AppendDefaultPath",
+			image:    defaultImage,
+			envOpt:   []string{"APPEND_PATH=/foo"},
+			matchEnv: "PATH",
+			matchVal: defaultPath + ":/foo",
+		},
+		{
+			name:     "PrependDefaultPath",
+			image:    defaultImage,
+			envOpt:   []string{"PREPEND_PATH=/foo"},
+			matchEnv: "PATH",
+			matchVal: "/foo:" + defaultPath,
+		},
+		{
+			name:     "TestMultiLine",
+			image:    defaultImage,
+			envOpt:   []string{"MULTI=Hello\nWorld"},
+			matchEnv: "MULTI",
+			matchVal: "Hello\nWorld",
+		},
+		{
+			name:     "TestEscapedNewline",
+			image:    defaultImage,
+			envOpt:   []string{"ESCAPED=Hello\\nWorld"},
+			matchEnv: "ESCAPED",
+			matchVal: "Hello\\nWorld",
+		},
+		{
+			name:     "TestInvalidKey",
+			image:    defaultImage,
+			envOpt:   []string{"BASH_FUNC_ml%%=TEST"},
+			matchEnv: "BASH_FUNC_ml%%",
+			matchVal: "",
+		},
+		{
+			name:     "TestDefaultLdLibraryPath",
+			image:    defaultImage,
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: singularityLibs,
+		},
+		{
+			name:     "TestCustomTrailingCommaPath",
+			image:    defaultImage,
+			envOpt:   []string{"LD_LIBRARY_PATH=/foo,"},
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: "/foo,:" + singularityLibs,
+		},
+		{
+			name:     "TestCustomLdLibraryPath",
+			image:    defaultImage,
+			envOpt:   []string{"LD_LIBRARY_PATH=/foo"},
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: "/foo:" + singularityLibs,
+		},
+		{
+			name:     "SINGULARITY_NAME",
+			image:    defaultImage,
+			matchEnv: "SINGULARITY_NAME",
+			matchVal: defaultImage,
+		},
+	}
+
+	for _, tt := range tests {
+		args := make([]string, 0)
+		if tt.envOpt != nil {
+			args = append(args, "--env", strings.Join(tt.envOpt, ","))
+		}
+		args = append(args, tt.image, "/bin/sh", "-c", "echo \"${"+tt.matchEnv+"}\"")
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(tt.name),
+			e2e.WithProfile(e2e.OCIUserProfile),
+			e2e.WithCommand("exec"),
+			e2e.WithEnv(tt.hostEnv),
+			e2e.WithArgs(args...),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.ExactMatch, tt.matchVal),
+			),
+		)
+	}
+}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/env/oci.go
@@ -1,0 +1,126 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package apptainerenv
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/apptainer/apptainer/e2e/internal/e2e"
+)
+
+func (c ctx) ociEnvOption(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+	defaultImage := "oci-archive:" + c.env.OCIImagePath
+
+	tests := []struct {
+		name     string
+		image    string
+		envOpt   []string
+		hostEnv  []string
+		matchEnv string
+		matchVal string
+	}{
+		{
+			name:     "DefaultPath",
+			image:    defaultImage,
+			matchEnv: "PATH",
+			matchVal: defaultPath,
+		},
+		{
+			name:     "DefaultPathOverride",
+			image:    defaultImage,
+			envOpt:   []string{"PATH=/"},
+			matchEnv: "PATH",
+			matchVal: "/",
+		},
+		{
+			name:     "AppendDefaultPath",
+			image:    defaultImage,
+			envOpt:   []string{"APPEND_PATH=/foo"},
+			matchEnv: "PATH",
+			matchVal: defaultPath + ":/foo",
+		},
+		{
+			name:     "PrependDefaultPath",
+			image:    defaultImage,
+			envOpt:   []string{"PREPEND_PATH=/foo"},
+			matchEnv: "PATH",
+			matchVal: "/foo:" + defaultPath,
+		},
+		{
+			name:     "TestMultiLine",
+			image:    defaultImage,
+			envOpt:   []string{"MULTI=Hello\nWorld"},
+			matchEnv: "MULTI",
+			matchVal: "Hello\nWorld",
+		},
+		{
+			name:     "TestEscapedNewline",
+			image:    defaultImage,
+			envOpt:   []string{"ESCAPED=Hello\\nWorld"},
+			matchEnv: "ESCAPED",
+			matchVal: "Hello\\nWorld",
+		},
+		{
+			name:     "TestInvalidKey",
+			image:    defaultImage,
+			envOpt:   []string{"BASH_FUNC_ml%%=TEST"},
+			matchEnv: "BASH_FUNC_ml%%",
+			matchVal: "",
+		},
+		{
+			name:     "TestDefaultLdLibraryPath",
+			image:    defaultImage,
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: apptainerLibs,
+		},
+		{
+			name:     "TestCustomTrailingCommaPath",
+			image:    defaultImage,
+			envOpt:   []string{"LD_LIBRARY_PATH=/foo,"},
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: "/foo,:" + apptainerLibs,
+		},
+		{
+			name:     "TestCustomLdLibraryPath",
+			image:    defaultImage,
+			envOpt:   []string{"LD_LIBRARY_PATH=/foo"},
+			matchEnv: "LD_LIBRARY_PATH",
+			matchVal: "/foo:" + apptainerLibs,
+		},
+		{
+			name:     "APPTAINER_NAME",
+			image:    defaultImage,
+			matchEnv: "APPTAINER_NAME",
+			matchVal: defaultImage,
+		},
+	}
+
+	for _, tt := range tests {
+		args := make([]string, 0)
+		if tt.envOpt != nil {
+			args = append(args, "--env", strings.Join(tt.envOpt, ","))
+		}
+		args = append(args, tt.image, "/bin/sh", "-c", "echo \"${"+tt.matchEnv+"}\"")
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(tt.name),
+			e2e.WithProfile(e2e.OCIUserProfile),
+			e2e.WithCommand("exec"),
+			e2e.WithEnv(tt.hostEnv),
+			e2e.WithArgs(args...),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.ExactMatch, tt.matchVal),
+			),
+		)
+	}
+}