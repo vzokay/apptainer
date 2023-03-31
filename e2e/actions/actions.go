sylabs/singularity

e2e: test --oci run/exec with args

e2e/actions/actions.go
@@ -2461,6 +2461,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun": c.actionOciRun, // singularity run --oci
+		"ociRun":  c.actionOciRun,  // singularity run --oci
+		"ociExec": c.actionOciExec, // singularity exec --oci
 	}
 }

e2e/actions/oci.go
@@ -6,11 +6,9 @@
 package actions
 
 import (
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/sylabs/singularity/e2e/internal/e2e"
@@ -18,46 +16,22 @@ import (
 
 const (
 	dockerArchiveURI = "https://s3.amazonaws.com/singularity-ci-public/alpine-docker-save.tar"
-	ociArchiveURI    = "https://s3.amazonaws.com/singularity-ci-public/alpine-oci-archive.tar"
 )
 
-func getTestTar(url string) (path string, err error) {
-	dl, err := os.CreateTemp("", "oci-test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dl.Close()
-
-	r, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer r.Body.Close()
-
-	_, err = io.Copy(dl, r.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return dl.Name(), nil
-}
-
 func (c actionTests) actionOciRun(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+
 	// Prepare docker-archive source
-	dockerArchive, err := getTestTar(dockerArchiveURI)
+	tmpDir := t.TempDir()
+	dockerArchive := filepath.Join(tmpDir, "docker-archive.tar")
+	err := e2e.DownloadFile(dockerArchiveURI, dockerArchive)
 	if err != nil {
 		t.Fatalf("Could not download docker archive test file: %v", err)
 	}
 	defer os.Remove(dockerArchive)
-	// Prepare oci-archive source
-	ociArchive, err := getTestTar(ociArchiveURI)
-	if err != nil {
-		t.Fatalf("Could not download oci archive test file: %v", err)
-	}
-	defer os.Remove(ociArchive)
 	// Prepare oci source (oci directory layout)
 	ociLayout := t.TempDir()
-	cmd := exec.Command("tar", "-C", ociLayout, "-xf", ociArchive)
+	cmd := exec.Command("tar", "-C", ociLayout, "-xf", c.env.OCIImagePath)
 	err = cmd.Run()
 	if err != nil {
 		t.Fatalf("Error extracting oci archive to layout: %v", err)
@@ -66,6 +40,7 @@ func (c actionTests) actionOciRun(t *testing.T) {
 	tests := []struct {
 		name     string
 		imageRef string
+		argv     []string
 		exit     int
 	}{
 		{
@@ -75,7 +50,7 @@ func (c actionTests) actionOciRun(t *testing.T) {
 		},
 		{
 			name:     "oci-archive",
-			imageRef: "oci-archive:" + ociArchive,
+			imageRef: "oci-archive:" + c.env.OCIImagePath,
 			exit:     0,
 		},
 		{
@@ -83,11 +58,25 @@ func (c actionTests) actionOciRun(t *testing.T) {
 			imageRef: "oci:" + ociLayout,
 			exit:     0,
 		},
+		{
+			name:     "true",
+			imageRef: "oci:" + ociLayout,
+			argv:     []string{"true"},
+			exit:     0,
+		},
+		{
+			name:     "false",
+			imageRef: "oci:" + ociLayout,
+			argv:     []string{"false"},
+			exit:     1,
+		},
 	}
 
 	for _, profile := range []e2e.Profile{e2e.OCIRootProfile, e2e.OCIUserProfile} {
 		t.Run(profile.String(), func(t *testing.T) {
 			for _, tt := range tests {
+				cmdArgs := []string{tt.imageRef}
+				cmdArgs = append(cmdArgs, tt.argv...)
 				c.env.RunSingularity(
 					t,
 					e2e.AsSubtest(tt.name),
@@ -95,10 +84,61 @@ func (c actionTests) actionOciRun(t *testing.T) {
 					e2e.WithCommand("run"),
 					// While we don't support args we are entering a /bin/sh interactively.
 					e2e.ConsoleRun(e2e.ConsoleSendLine("exit")),
-					e2e.WithArgs(tt.imageRef),
+					e2e.WithArgs(cmdArgs...),
 					e2e.ExpectExit(tt.exit),
 				)
 			}
 		})
+	}
+}
+
+// exec tests min fuctionality for singularity exec
+func (c actionTests) actionOciExec(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+
+	imageRef := "oci-archive:" + c.env.OCIImagePath
+
+	tests := []struct {
+		name string
+		argv []string
+		exit int
+	}{
+		{
+			name: "NoCommand",
+			argv: []string{imageRef},
+			exit: 1,
+		},
+		{
+			name: "True",
+			argv: []string{imageRef, "true"},
+			exit: 0,
+		},
+		{
+			name: "TrueAbsPAth",
+			argv: []string{imageRef, "/bin/true"},
+			exit: 0,
+		},
+		{
+			name: "False",
+			argv: []string{imageRef, "false"},
+			exit: 1,
+		},
+		{
+			name: "FalseAbsPath",
+			argv: []string{imageRef, "/bin/false"},
+			exit: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(tt.name),
+			e2e.WithProfile(e2e.UserProfile),
+			e2e.WithCommand("exec"),
+			e2e.WithDir("/tmp"),
+			e2e.WithArgs(tt.argv...),
+			e2e.ExpectExit(tt.exit),
+		)
 	}
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/actions.go
@@ -2559,6 +2559,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun": c.actionOciRun, // apptainer run --oci
+		"ociRun":  c.actionOciRun,  // apptainer run --oci
+		"ociExec": c.actionOciExec, // apptainer exec --oci
 	}
 }

e2e/actions/oci.go
@@ -10,11 +10,9 @@
 package actions
 
 import (
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"testing"
 
 	"github.com/apptainer/apptainer/e2e/internal/e2e"
@@ -22,46 +20,22 @@ import (
 
 const (
 	dockerArchiveURI = "https://github.com/apptainer/apptainer/releases/download/v0.1.0/alpine-docker-save.tar"
-	ociArchiveURI    = "https://github.com/apptainer/apptainer/releases/download/v0.1.0/alpine-oci-archive.tar"
 )
 
-func getTestTar(url string) (path string, err error) {
-	dl, err := os.CreateTemp("", "oci-test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dl.Close()
-
-	r, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer r.Body.Close()
-
-	_, err = io.Copy(dl, r.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return dl.Name(), nil
-}
-
 func (c actionTests) actionOciRun(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+
 	// Prepare docker-archive source
-	dockerArchive, err := getTestTar(dockerArchiveURI)
+	tmpDir := t.TempDir()
+	dockerArchive := filepath.Join(tmpDir, "docker-archive.tar")
+	err := e2e.DownloadFile(dockerArchiveURI, dockerArchive)
 	if err != nil {
 		t.Fatalf("Could not download docker archive test file: %v", err)
 	}
 	defer os.Remove(dockerArchive)
-	// Prepare oci-archive source
-	ociArchive, err := getTestTar(ociArchiveURI)
-	if err != nil {
-		t.Fatalf("Could not download oci archive test file: %v", err)
-	}
-	defer os.Remove(ociArchive)
 	// Prepare oci source (oci directory layout)
 	ociLayout := t.TempDir()
-	cmd := exec.Command("tar", "-C", ociLayout, "-xf", ociArchive)
+	cmd := exec.Command("tar", "-C", ociLayout, "-xf", c.env.OCIImagePath)
 	err = cmd.Run()
 	if err != nil {
 		t.Fatalf("Error extracting oci archive to layout: %v", err)
@@ -70,6 +44,7 @@ func (c actionTests) actionOciRun(t *testing.T) {
 	tests := []struct {
 		name     string
 		imageRef string
+		argv     []string
 		exit     int
 	}{
 		{
@@ -79,7 +54,7 @@ func (c actionTests) actionOciRun(t *testing.T) {
 		},
 		{
 			name:     "oci-archive",
-			imageRef: "oci-archive:" + ociArchive,
+			imageRef: "oci-archive:" + c.env.OCIImagePath,
 			exit:     0,
 		},
 		{
@@ -87,11 +62,25 @@ func (c actionTests) actionOciRun(t *testing.T) {
 			imageRef: "oci:" + ociLayout,
 			exit:     0,
 		},
+		{
+			name:     "true",
+			imageRef: "oci:" + ociLayout,
+			argv:     []string{"true"},
+			exit:     0,
+		},
+		{
+			name:     "false",
+			imageRef: "oci:" + ociLayout,
+			argv:     []string{"false"},
+			exit:     1,
+		},
 	}
 
 	for _, profile := range []e2e.Profile{e2e.OCIRootProfile, e2e.OCIUserProfile} {
 		t.Run(profile.String(), func(t *testing.T) {
 			for _, tt := range tests {
+				cmdArgs := []string{tt.imageRef}
+				cmdArgs = append(cmdArgs, tt.argv...)
 				c.env.RunApptainer(
 					t,
 					e2e.AsSubtest(tt.name),
@@ -99,10 +88,61 @@ func (c actionTests) actionOciRun(t *testing.T) {
 					e2e.WithCommand("run"),
 					// While we don't support args we are entering a /bin/sh interactively.
 					e2e.ConsoleRun(e2e.ConsoleSendLine("exit")),
-					e2e.WithArgs(tt.imageRef),
+					e2e.WithArgs(cmdArgs...),
 					e2e.ExpectExit(tt.exit),
 				)
 			}
 		})
+	}
+}
+
+// exec tests min fuctionality for apptainer exec
+func (c actionTests) actionOciExec(t *testing.T) {
+	e2e.EnsureOCIImage(t, c.env)
+
+	imageRef := "oci-archive:" + c.env.OCIImagePath
+
+	tests := []struct {
+		name string
+		argv []string
+		exit int
+	}{
+		{
+			name: "NoCommand",
+			argv: []string{imageRef},
+			exit: 1,
+		},
+		{
+			name: "True",
+			argv: []string{imageRef, "true"},
+			exit: 0,
+		},
+		{
+			name: "TrueAbsPAth",
+			argv: []string{imageRef, "/bin/true"},
+			exit: 0,
+		},
+		{
+			name: "False",
+			argv: []string{imageRef, "false"},
+			exit: 1,
+		},
+		{
+			name: "FalseAbsPath",
+			argv: []string{imageRef, "/bin/false"},
+			exit: 1,
+		},
+	}
+
+	for _, tt := range tests {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(tt.name),
+			e2e.WithProfile(e2e.UserProfile),
+			e2e.WithCommand("exec"),
+			e2e.WithDir("/tmp"),
+			e2e.WithArgs(tt.argv...),
+			e2e.ExpectExit(tt.exit),
+		)
 	}
 }