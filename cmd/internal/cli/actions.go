sylabs/singularity

actions: Add --oci flag to call OCI launcher

cmd/internal/cli/actions.go
@@ -23,6 +23,7 @@ import (
 	"github.com/sylabs/singularity/internal/pkg/client/shub"
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher"
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher/native"
+	ocilauncher "github.com/sylabs/singularity/internal/pkg/runtime/launcher/oci"
 	"github.com/sylabs/singularity/internal/pkg/util/uri"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
@@ -335,9 +336,18 @@ func launchContainer(cmd *cobra.Command, image string, args []string, instanceNa
 	// Explicitly use the interface type here, as we will add alternative launchers later...
 	var l launcher.Launcher
 
-	l, err = native.NewLauncher(opts...)
-	if err != nil {
-		return fmt.Errorf("while configuring container: %s", err)
+	if ociRuntime {
+		sylog.Debugf("Using OCI runtime launcher.")
+		l, err = ocilauncher.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
+	} else {
+		sylog.Debugf("Using native runtime launcher.")
+		l, err = native.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
 	}
 
 	return l.Exec(cmd.Context(), image, args, instanceName)

e2e/actions/actions.go
@@ -903,7 +903,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1426,7 +1426,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2338,6 +2338,24 @@ func countSquashfuseMounts(t *testing.T) int {
 	return count
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2379,6 +2397,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"umask":                 c.actionUmask,         // test umask propagation
 		"no-mount":              c.actionNoMount,       // test --no-mount
 		"compat":                c.actionCompat,        // test --compat
+		"ociRuntime":            c.ociRuntime,          // test --oci (unimplemented)
 		"invalidRemote":         np(c.invalidRemote),   // GHSA-5mv9-q7fq-9394
 		"SIFFUSE":               np(c.actionSIFFUSE),   // test --sif-fuse
 		"NoSIFFUSE":             np(c.actionNoSIFFUSE), // test absence of squashfs and CleanupHost()

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/actions.go
@@ -26,6 +26,7 @@ import (
 	"github.com/apptainer/apptainer/internal/pkg/client/shub"
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher"
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher/native"
+	ocilauncher "github.com/apptainer/apptainer/internal/pkg/runtime/launcher/oci"
 	"github.com/apptainer/apptainer/internal/pkg/util/env"
 	"github.com/apptainer/apptainer/internal/pkg/util/uri"
 	"github.com/apptainer/apptainer/pkg/sylog"
@@ -362,12 +363,20 @@ func launchContainer(cmd *cobra.Command, image string, args []string, instanceNa
 		launcher.OptTmpDir(tmpDir),
 	}
 
-	// Explicitly use the interface type here, as we will add alternative launchers later...
 	var l launcher.Launcher
 
-	l, err = native.NewLauncher(opts...)
-	if err != nil {
-		return fmt.Errorf("while configuring container: %s", err)
+	if ociRuntime {
+		sylog.Debugf("Using OCI runtime launcher.")
+		l, err = ocilauncher.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
+	} else {
+		sylog.Debugf("Using native runtime launcher.")
+		l, err = native.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
 	}
 
 	return l.Exec(cmd.Context(), image, args, instanceName)

e2e/actions/actions.go
@@ -917,7 +917,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1551,7 +1551,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2509,6 +2509,24 @@ func (c actionTests) actionFakerootHome(t *testing.T) {
 	}
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2554,6 +2572,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"no-mount":                  c.actionNoMount,           // test --no-mount
 		"compat":                    np(c.actionCompat),        // test --compat
 		"umask":                     np(c.actionUmask),         // test umask propagation
+		"ociRuntime":                c.ociRuntime,              // test --oci (unimplemented)
 		"invalidRemote":             np(c.invalidRemote),       // GHSA-5mv9-q7fq-9394
 		"fakeroot home":             c.actionFakerootHome,      // test home dir in fakeroot
 	}