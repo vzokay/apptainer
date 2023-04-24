sylabs/singularity

fix: oci: honour config resolv_conf directive

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -364,6 +364,11 @@ func (l *Launcher) prepareResolvConf(rootfs string) error {
 	containerEtc := filepath.Join(rootfs, "etc")
 	containerResolvConfPath := filepath.Join(rootfs, "etc", "resolv.conf")
 
+	if !l.singularityConf.ConfigResolvConf {
+		sylog.Debugf("Skipping update of %s due to singularity.conf", containerResolvConfPath)
+		return nil
+	}
+
 	var resolvConfData []byte
 	var err error
 	if len(l.cfg.DNS) > 0 {

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -365,6 +365,11 @@ func (l *Launcher) prepareResolvConf(rootfs string) error {
 	containerEtc := filepath.Join(rootfs, "etc")
 	containerResolvConfPath := filepath.Join(rootfs, "etc", "resolv.conf")
 
+	if !l.apptainerConf.ConfigResolvConf {
+		sylog.Debugf("Skipping update of %s due to apptainer.conf", containerResolvConfPath)
+		return nil
+	}
+
 	var resolvConfData []byte
 	var err error
 	if len(l.cfg.DNS) > 0 {