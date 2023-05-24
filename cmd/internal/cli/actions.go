sylabs/singularity

actions: Add --oci flag to call OCI launcher

cmd/internal/cli/actions.go
@@ -23,6 +23,7 @@ import (
 	"github.com/sylabs/singularity/internal/pkg/client/shub"
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher"
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher/native"
+	ocilauncher "github.com/sylabs/singularity/internal/pkg/runtime/launcher/oci"
 	"github.com/sylabs/singularity/internal/pkg/util/uri"
 	"github.com/sylabs/singularity/pkg/sylog"
 )
@@ -335,9 +336,18 @@ func launchContainer(cmd *cobra.Command, image string, args []string, instanceNa
 	// Explicitly use the interface type here, as we will add alternative launchers later...
 	var l launcher.Launcher
 
-	l, err = native.NewLauncher(opts...)
-	if err != nil {
-		return fmt.Errorf("while configuring container: %s", err)
+	if ociRuntime {
+		sylog.Debugf("Using OCI runtime launcher.")
+		l, err = ocilauncher.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
+	} else {
+		sylog.Debugf("Using native runtime launcher.")
+		l, err = native.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
 	}
 
 	return l.Exec(cmd.Context(), image, args, instanceName)

e2e/actions/actions.go
@@ -903,7 +903,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1426,7 +1426,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2338,6 +2338,24 @@ func countSquashfuseMounts(t *testing.T) int {
 	return count
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2379,6 +2397,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"umask":                 c.actionUmask,         // test umask propagation
 		"no-mount":              c.actionNoMount,       // test --no-mount
 		"compat":                c.actionCompat,        // test --compat
+		"ociRuntime":            c.ociRuntime,          // test --oci (unimplemented)
 		"invalidRemote":         np(c.invalidRemote),   // GHSA-5mv9-q7fq-9394
 		"SIFFUSE":               np(c.actionSIFFUSE),   // test --sif-fuse
 		"NoSIFFUSE":             np(c.actionNoSIFFUSE), // test absence of squashfs and CleanupHost()

e2e/internal/e2e/profile.go
@@ -20,19 +20,29 @@ const (
 	fakerootProfile          = "FakerootProfile"
 	userNamespaceProfile     = "UserNamespaceProfile"
 	rootUserNamespaceProfile = "RootUserNamespaceProfile"
+	ociUserProfile           = "OCIUserProfile"
+	ociRootProfile           = "OCIRootProfile"
+	ociFakerootProfile       = "OCIFakerootProfile"
 )
 
 var (
-	// UserProfile is the execution profile for a regular user.
-	UserProfile = Profiles[userProfile]
-	// RootProfile is the execution profile for root.
-	RootProfile = Profiles[rootProfile]
-	// FakerootProfile is the execution profile for fakeroot.
-	FakerootProfile = Profiles[fakerootProfile]
-	// UserNamespaceProfile is the execution profile for a regular user and a user namespace.
-	UserNamespaceProfile = Profiles[userNamespaceProfile]
-	// RootUserNamespaceProfile is the execution profile for root and a user namespace.
-	RootUserNamespaceProfile = Profiles[rootUserNamespaceProfile]
+	// UserProfile is the execution profile for a regular user, using the Singularity native runtime.
+	UserProfile = NativeProfiles[userProfile]
+	// RootProfile is the execution profile for root, using the Singularity native runtime.
+	RootProfile = NativeProfiles[rootProfile]
+	// FakerootProfile is the execution profile for fakeroot, using the Singularity native runtime.
+	FakerootProfile = NativeProfiles[fakerootProfile]
+	// UserNamespaceProfile is the execution profile for a regular user and a user namespace, using the Singularity native runtime.
+	UserNamespaceProfile = NativeProfiles[userNamespaceProfile]
+	// RootUserNamespaceProfile is the execution profile for root and a user namespace, using the Singularity native runtime.
+	RootUserNamespaceProfile = NativeProfiles[rootUserNamespaceProfile]
+	// OCIUserProfile is the execution profile for a regular user, using the Singularity native runtime.
+	OCIUserProfile = OCIProfiles[ociUserProfile]
+	// RootProfile is the execution profile for root, using the Singularity native runtime.
+	OCIRootProfile = OCIProfiles[ociRootProfile]
+	// FakerootProfile is the execution profile for fakeroot, using the Singularity native runtime.
+	OCIFakerootProfile = OCIProfiles[ociFakerootProfile]
+	// UserNamespaceProfile is the execution profile for a regular user and a user namespace, using the Singularity native runtime.
 )
 
 // Profile represents various properties required to run an E2E test
@@ -56,8 +66,8 @@ type Profile struct {
 	optionForCommands []string         // singularity commands concerned by the option to be added
 }
 
-// Profiles defines all available profiles.
-var Profiles = map[string]Profile{
+// NativeProfiles defines all available profiles for the native singularity runtime
+var NativeProfiles = map[string]Profile{
 	userProfile: {
 		name:              "User",
 		privileged:        false,
@@ -106,6 +116,40 @@ var Profiles = map[string]Profile{
 		defaultCwd:        "/root", // need to run in a directory owned by root
 		requirementsFn:    require.UserNamespace,
 		singularityOption: "--userns",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+}
+
+// OCIProfiles defines all available profiles for the OCI runtime
+var OCIProfiles = map[string]Profile{
+	ociUserProfile: {
+		name:              "OCIUser",
+		privileged:        false,
+		hostUID:           origUID,
+		containerUID:      origUID,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		singularityOption: "--oci",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+	ociRootProfile: {
+		name:              "OCIRoot",
+		privileged:        true,
+		hostUID:           0,
+		containerUID:      0,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		singularityOption: "--oci",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+	ociFakerootProfile: {
+		name:              "OCIFakeroot",
+		privileged:        false,
+		hostUID:           origUID,
+		containerUID:      0,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		singularityOption: "--oci --fakeroot",
 		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
 	},
 }
@@ -184,6 +228,29 @@ func (p Profile) String() string {
 // correctly execute commands with the fakeroot profile.
 func fakerootRequirements(t *testing.T) {
 	require.UserNamespace(t)
+
+	uid := uint32(origUID)
+
+	// check that current user has valid mappings in /etc/subuid
+	if _, err := fakeroot.GetIDRange(fakeroot.SubUIDFile, uid); err != nil {
+		t.Fatalf("fakeroot configuration error: %s", err)
+	}
+
+	// check that current user has valid mappings in /etc/subgid;
+	// since that file contains the group mappings for a given user
+	// *name*, it is keyed by user name, not by group name. This
+	// means that even if we are requesting the *group* mappings, we
+	// need to pass the *user* ID.
+	if _, err := fakeroot.GetIDRange(fakeroot.SubGIDFile, uid); err != nil {
+		t.Fatalf("fakeroot configuration error: %s", err)
+	}
+}
+
+// ociRequirements ensures requirements are satisfied to correctly execute
+// commands with the OCI runtime / profile.
+func ociRequirements(t *testing.T) {
+	require.UserNamespace(t)
+	require.Command(t, "runc")
 
 	uid := uint32(origUID)
 

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

cmd/internal/cli/actions.go
@@ -26,6 +26,7 @@ import (
 	"github.com/apptainer/apptainer/internal/pkg/client/shub"
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher"
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher/native"
+	ocilauncher "github.com/apptainer/apptainer/internal/pkg/runtime/launcher/oci"
 	"github.com/apptainer/apptainer/internal/pkg/util/env"
 	"github.com/apptainer/apptainer/internal/pkg/util/uri"
 	"github.com/apptainer/apptainer/pkg/sylog"
@@ -363,12 +364,20 @@ func launchContainer(cmd *cobra.Command, image string, args []string, instanceNa
 		launcher.OptUnderlay(underlay),
 	}
 
-	// Explicitly use the interface type here, as we will add alternative launchers later...
 	var l launcher.Launcher
 
-	l, err = native.NewLauncher(opts...)
-	if err != nil {
-		return fmt.Errorf("while configuring container: %s", err)
+	if ociRuntime {
+		sylog.Debugf("Using OCI runtime launcher.")
+		l, err = ocilauncher.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
+	} else {
+		sylog.Debugf("Using native runtime launcher.")
+		l, err = native.NewLauncher(opts...)
+		if err != nil {
+			return fmt.Errorf("while configuring container: %s", err)
+		}
 	}
 
 	return l.Exec(cmd.Context(), image, args, instanceName)

e2e/actions/actions.go
@@ -917,7 +917,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1818,7 +1818,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2776,6 +2776,24 @@ func (c actionTests) actionFakerootHome(t *testing.T) {
 	}
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2821,6 +2839,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"no-mount":                  c.actionNoMount,           // test --no-mount
 		"compat":                    np(c.actionCompat),        // test --compat
 		"umask":                     np(c.actionUmask),         // test umask propagation
+		"ociRuntime":                c.ociRuntime,              // test --oci (unimplemented)
 		"invalidRemote":             np(c.invalidRemote),       // GHSA-5mv9-q7fq-9394
 		"fakeroot home":             c.actionFakerootHome,      // test home dir in fakeroot
 	}

e2e/internal/e2e/profile.go
@@ -24,19 +24,29 @@ const (
 	fakerootProfile          = "FakerootProfile"
 	userNamespaceProfile     = "UserNamespaceProfile"
 	rootUserNamespaceProfile = "RootUserNamespaceProfile"
+	ociUserProfile           = "OCIUserProfile"
+	ociRootProfile           = "OCIRootProfile"
+	ociFakerootProfile       = "OCIFakerootProfile"
 )
 
 var (
-	// UserProfile is the execution profile for a regular user.
-	UserProfile = Profiles[userProfile]
-	// RootProfile is the execution profile for root.
-	RootProfile = Profiles[rootProfile]
-	// FakerootProfile is the execution profile for fakeroot.
-	FakerootProfile = Profiles[fakerootProfile]
-	// UserNamespaceProfile is the execution profile for a regular user and a user namespace.
-	UserNamespaceProfile = Profiles[userNamespaceProfile]
-	// RootUserNamespaceProfile is the execution profile for root and a user namespace.
-	RootUserNamespaceProfile = Profiles[rootUserNamespaceProfile]
+	// UserProfile is the execution profile for a regular user, using the Apptainer native runtime.
+	UserProfile = NativeProfiles[userProfile]
+	// RootProfile is the execution profile for root, using the Apptainer native runtime.
+	RootProfile = NativeProfiles[rootProfile]
+	// FakerootProfile is the execution profile for fakeroot, using the Apptainer native runtime.
+	FakerootProfile = NativeProfiles[fakerootProfile]
+	// UserNamespaceProfile is the execution profile for a regular user and a user namespace, using the Apptainer native runtime.
+	UserNamespaceProfile = NativeProfiles[userNamespaceProfile]
+	// RootUserNamespaceProfile is the execution profile for root and a user namespace, using the Apptainer native runtime.
+	RootUserNamespaceProfile = NativeProfiles[rootUserNamespaceProfile]
+	// OCIUserProfile is the execution profile for a regular user, using the Apptainer native runtime.
+	OCIUserProfile = OCIProfiles[ociUserProfile]
+	// RootProfile is the execution profile for root, using the Apptainer native runtime.
+	OCIRootProfile = OCIProfiles[ociRootProfile]
+	// FakerootProfile is the execution profile for fakeroot, using the Apptainer native runtime.
+	OCIFakerootProfile = OCIProfiles[ociFakerootProfile]
+	// UserNamespaceProfile is the execution profile for a regular user and a user namespace, using the Apptainer native runtime.
 )
 
 // Profile represents various properties required to run an E2E test
@@ -60,8 +70,8 @@ type Profile struct {
 	optionForCommands []string         // apptainer commands concerned by the option to be added
 }
 
-// Profiles defines all available profiles.
-var Profiles = map[string]Profile{
+// NativeProfiles defines all available profiles for the native apptainer runtime
+var NativeProfiles = map[string]Profile{
 	userProfile: {
 		name:              "User",
 		privileged:        false,
@@ -110,6 +120,40 @@ var Profiles = map[string]Profile{
 		defaultCwd:        "/root", // need to run in a directory owned by root
 		requirementsFn:    require.UserNamespace,
 		apptainerOption:   "--userns",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+}
+
+// OCIProfiles defines all available profiles for the OCI runtime
+var OCIProfiles = map[string]Profile{
+	ociUserProfile: {
+		name:              "OCIUser",
+		privileged:        false,
+		hostUID:           origUID,
+		containerUID:      origUID,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		apptainerOption:   "--oci",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+	ociRootProfile: {
+		name:              "OCIRoot",
+		privileged:        true,
+		hostUID:           0,
+		containerUID:      0,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		apptainerOption:   "--oci",
+		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
+	},
+	ociFakerootProfile: {
+		name:              "OCIFakeroot",
+		privileged:        false,
+		hostUID:           origUID,
+		containerUID:      0,
+		defaultCwd:        "",
+		requirementsFn:    ociRequirements,
+		apptainerOption:   "--oci --fakeroot",
 		optionForCommands: []string{"shell", "exec", "run", "test", "instance start"},
 	},
 }
@@ -188,6 +232,29 @@ func (p Profile) String() string {
 // correctly execute commands with the fakeroot profile.
 func fakerootRequirements(t *testing.T) {
 	require.UserNamespace(t)
+
+	uid := uint32(origUID)
+
+	// check that current user has valid mappings in /etc/subuid
+	if _, err := fakeroot.GetIDRange(fakeroot.SubUIDFile, uid); err != nil {
+		t.Fatalf("fakeroot configuration error: %s", err)
+	}
+
+	// check that current user has valid mappings in /etc/subgid;
+	// since that file contains the group mappings for a given user
+	// *name*, it is keyed by user name, not by group name. This
+	// means that even if we are requesting the *group* mappings, we
+	// need to pass the *user* ID.
+	if _, err := fakeroot.GetIDRange(fakeroot.SubGIDFile, uid); err != nil {
+		t.Fatalf("fakeroot configuration error: %s", err)
+	}
+}
+
+// ociRequirements ensures requirements are satisfied to correctly execute
+// commands with the OCI runtime / profile.
+func ociRequirements(t *testing.T) {
+	require.UserNamespace(t)
+	require.Command(t, "runc")
 
 	uid := uint32(origUID)
 