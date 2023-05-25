sylabs/singularity

added unit- and e2e-tests for revamped Passwd()

e2e/actions/oci.go
@@ -235,6 +235,14 @@ func (c actionTests) actionOciExec(t *testing.T) {
 				e2e.ExpectOutput(e2e.RegexMatch, `^(\s*)Server:(\s+)(1\.1\.1\.1)(\s*)\n`),
 			},
 		},
+		{
+			name: "CustomHomePreservesRootShell",
+			argv: []string{"--home", "/tmp", imageRef, "cat", "/etc/passwd"},
+			exit: 0,
+			wantOutputs: []e2e.SingularityCmdResultOp{
+				e2e.ExpectOutput(e2e.RegexMatch, `^root:x:0:0:root:[^:]*:/bin/sh\n`),
+			},
+		},
 	}
 	for _, profile := range e2e.OCIProfiles {
 		t.Run(profile.String(), func(t *testing.T) {

internal/pkg/util/fs/files/passwd_test.go
@@ -1,0 +1,53 @@
+// Copyright (c) 2018-2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package files
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/sylabs/singularity/internal/pkg/test"
+	"gotest.tools/v3/golden"
+)
+
+func TestPasswd(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	uid := os.Getuid()
+
+	// Test how Passwd() works with a bad passwd file
+	_, err := Passwd("/fake", "/fake", uid)
+	if err == nil {
+		t.Errorf("should have failed with bad passwd file")
+	}
+
+	// Test how Passwd() works with an empty file
+	f, err := os.CreateTemp("", "empty-passwd-")
+	if err != nil {
+		t.Error(err)
+	}
+	emptyPasswd := f.Name()
+	defer os.Remove(emptyPasswd)
+	f.Close()
+	_, err = Passwd(emptyPasswd, "/home", uid)
+	if err != nil {
+		t.Error(err)
+	}
+
+	inputPasswdFilePath := filepath.Join(".", "testdata", "passwd.in")
+	testUID := 0
+	testHomeDir := "/tmp"
+	testGoldenFile := "passwd.root.customhome.golden"
+	bytes, err := Passwd(inputPasswdFilePath, testHomeDir, testUID)
+	if err != nil {
+		t.Errorf("Unexpected error encountered calling Passwd(): %v", err)
+		return
+	}
+
+	golden.Assert(t, string(bytes), testGoldenFile, "mismatch in Passwd() invocation (uid: %d; requested homeDir: %#v)", testUID, testHomeDir)
+}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/oci.go
@@ -231,6 +231,14 @@ func (c actionTests) actionOciExec(t *testing.T) {
 				e2e.ExpectOutput(e2e.RegexMatch, `^(\s*)Server:(\s+)(1\.1\.1\.1)(\s*)\n`),
 			},
 		},
+		{
+			name: "CustomHomePreservesRootShell",
+			argv: []string{"--home", "/tmp", imageRef, "cat", "/etc/passwd"},
+			exit: 0,
+			wantOutputs: []e2e.ApptainerCmdResultOp{
+				e2e.ExpectOutput(e2e.RegexMatch, `^root:x:0:0:root:[^:]*:/bin/sh\n`),
+			},
+		},
 	}
 	for _, profile := range e2e.OCIProfiles {
 		t.Run(profile.String(), func(t *testing.T) {

internal/pkg/util/fs/files/passwd_test.go
@@ -1,0 +1,57 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2018-2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package files
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/apptainer/apptainer/internal/pkg/test"
+	"gotest.tools/v3/golden"
+)
+
+func TestPasswd(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	uid := os.Getuid()
+
+	// Test how Passwd() works with a bad passwd file
+	_, err := Passwd("/fake", "/fake", uid)
+	if err == nil {
+		t.Errorf("should have failed with bad passwd file")
+	}
+
+	// Test how Passwd() works with an empty file
+	f, err := os.CreateTemp("", "empty-passwd-")
+	if err != nil {
+		t.Error(err)
+	}
+	emptyPasswd := f.Name()
+	defer os.Remove(emptyPasswd)
+	f.Close()
+	_, err = Passwd(emptyPasswd, "/home", uid)
+	if err != nil {
+		t.Error(err)
+	}
+
+	inputPasswdFilePath := filepath.Join(".", "testdata", "passwd.in")
+	testUID := 0
+	testHomeDir := "/tmp"
+	testGoldenFile := "passwd.root.customhome.golden"
+	bytes, err := Passwd(inputPasswdFilePath, testHomeDir, testUID)
+	if err != nil {
+		t.Errorf("Unexpected error encountered calling Passwd(): %v", err)
+		return
+	}
+
+	golden.Assert(t, string(bytes), testGoldenFile, "mismatch in Passwd() invocation (uid: %d; requested homeDir: %#v)", testUID, testHomeDir)
+}