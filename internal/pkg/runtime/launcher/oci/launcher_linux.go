sylabs/singularity

fix: oci: honour config passwd / config group directives
Ensure that the `config passwd` and `config group` directives in
`singularity.conf` are honoured in `--oci` mode.

Add e2e config tests for OCI mode that test this.

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -332,20 +332,28 @@ func (l *Launcher) updatePasswdGroup(rootfs string, uid, gid uint32) error {
 	containerPasswd := filepath.Join(rootfs, "etc", "passwd")
 	containerGroup := filepath.Join(rootfs, "etc", "group")
 
-	sylog.Debugf("Updating passwd file: %s", containerPasswd)
-	content, err := files.Passwd(containerPasswd, l.cfg.HomeDir, int(uid))
-	if err != nil {
-		sylog.Warningf("%s", err)
-	} else if err := os.WriteFile(containerPasswd, content, 0o755); err != nil {
-		return fmt.Errorf("while writing passwd file: %w", err)
+	if l.singularityConf.ConfigPasswd {
+		sylog.Debugf("Updating passwd file: %s", containerPasswd)
+		content, err := files.Passwd(containerPasswd, l.cfg.HomeDir, int(uid))
+		if err != nil {
+			sylog.Warningf("%s", err)
+		} else if err := os.WriteFile(containerPasswd, content, 0o755); err != nil {
+			return fmt.Errorf("while writing passwd file: %w", err)
+		}
+	} else {
+		sylog.Debugf("Skipping update of %s due to singularity.conf", containerPasswd)
 	}
 
-	sylog.Debugf("Updating group file: %s", containerGroup)
-	content, err = files.Group(containerGroup, int(uid), []int{int(gid)})
-	if err != nil {
-		sylog.Warningf("%s", err)
-	} else if err := os.WriteFile(containerGroup, content, 0o755); err != nil {
-		return fmt.Errorf("while writing passwd file: %w", err)
+	if l.singularityConf.ConfigGroup {
+		sylog.Debugf("Updating group file: %s", containerGroup)
+		content, err := files.Group(containerGroup, int(uid), []int{int(gid)})
+		if err != nil {
+			sylog.Warningf("%s", err)
+		} else if err := os.WriteFile(containerGroup, content, 0o755); err != nil {
+			return fmt.Errorf("while writing passwd file: %w", err)
+		}
+	} else {
+		sylog.Debugf("Skipping update of %s due to singularity.conf", containerGroup)
 	}
 
 	return nil

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -333,20 +333,28 @@ func (l *Launcher) updatePasswdGroup(rootfs string, uid, gid uint32) error {
 	containerPasswd := filepath.Join(rootfs, "etc", "passwd")
 	containerGroup := filepath.Join(rootfs, "etc", "group")
 
-	sylog.Debugf("Updating passwd file: %s", containerPasswd)
-	content, err := files.Passwd(containerPasswd, l.cfg.HomeDir, int(uid))
-	if err != nil {
-		sylog.Warningf("%s", err)
-	} else if err := os.WriteFile(containerPasswd, content, 0o755); err != nil {
-		return fmt.Errorf("while writing passwd file: %w", err)
+	if l.apptainerConf.ConfigPasswd {
+		sylog.Debugf("Updating passwd file: %s", containerPasswd)
+		content, err := files.Passwd(containerPasswd, l.cfg.HomeDir, int(uid))
+		if err != nil {
+			sylog.Warningf("%s", err)
+		} else if err := os.WriteFile(containerPasswd, content, 0o755); err != nil {
+			return fmt.Errorf("while writing passwd file: %w", err)
+		}
+	} else {
+		sylog.Debugf("Skipping update of %s due to apptainer.conf", containerPasswd)
 	}
 
-	sylog.Debugf("Updating group file: %s", containerGroup)
-	content, err = files.Group(containerGroup, int(uid), []int{int(gid)})
-	if err != nil {
-		sylog.Warningf("%s", err)
-	} else if err := os.WriteFile(containerGroup, content, 0o755); err != nil {
-		return fmt.Errorf("while writing passwd file: %w", err)
+	if l.apptainerConf.ConfigGroup {
+		sylog.Debugf("Updating group file: %s", containerGroup)
+		content, err := files.Group(containerGroup, int(uid), []int{int(gid)})
+		if err != nil {
+			sylog.Warningf("%s", err)
+		} else if err := os.WriteFile(containerGroup, content, 0o755); err != nil {
+			return fmt.Errorf("while writing passwd file: %w", err)
+		}
+	} else {
+		sylog.Debugf("Skipping update of %s due to apptainer.conf", containerGroup)
 	}
 
 	return nil