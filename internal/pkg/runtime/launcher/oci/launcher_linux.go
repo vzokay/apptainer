sylabs/singularity

oci: inspection of image user in bundle

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -26,6 +26,7 @@ import (
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher"
 	"github.com/sylabs/singularity/internal/pkg/util/fs/files"
 	"github.com/sylabs/singularity/internal/pkg/util/user"
+	"github.com/sylabs/singularity/pkg/ocibundle"
 	"github.com/sylabs/singularity/pkg/ocibundle/native"
 	"github.com/sylabs/singularity/pkg/ocibundle/tools"
 	"github.com/sylabs/singularity/pkg/syfs"
@@ -254,6 +255,27 @@ func (l *Launcher) createSpec() (*specs.Spec, error) {
 	return &spec, nil
 }
 
+func (l *Launcher) updateSpecFromImage(ctx context.Context, b ocibundle.Bundle, spec *specs.Spec, image string, process string, args []string) error {
+	imgSpec := b.ImageSpec()
+	if imgSpec == nil {
+		return fmt.Errorf("bundle has no image spec")
+	}
+
+	specProcess, err := l.getProcess(ctx, *imgSpec, image, b.Path(), process, args)
+	if err != nil {
+		return err
+	}
+	spec.Process = specProcess
+	if err := b.Update(ctx, spec); err != nil {
+		return err
+	}
+
+	if err := l.updatePasswdGroup(tools.RootFs(b.Path()).Path()); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (l *Launcher) updatePasswdGroup(rootfs string) error {
 	uid := os.Getuid()
 	gid := os.Getgid()
@@ -355,18 +377,7 @@ func (l *Launcher) Exec(ctx context.Context, image string, process string, args 
 	}
 
 	// With reference to the bundle's image spec, now set the process configuration.
-	imgSpec := b.ImageSpec()
-	if imgSpec == nil {
-		return fmt.Errorf("bundle has no image spec")
-	}
-	specProcess, err := l.getProcess(ctx, *imgSpec, image, b.Path(), process, args)
-	if err != nil {
-		return err
-	}
-	spec.Process = specProcess
-	b.Update(ctx, spec)
-
-	if err := l.updatePasswdGroup(tools.RootFs(b.Path()).Path()); err != nil {
+	if err := l.updateSpecFromImage(ctx, b, spec, image, process, args); err != nil {
 		return err
 	}
 

pkg/ocibundle/tools/oci.go
@@ -8,12 +8,15 @@ package tools
 import (
 	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"syscall"
 
 	specs "github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/sylabs/singularity/internal/pkg/runtime/engine/config/oci"
 	"github.com/sylabs/singularity/internal/pkg/runtime/engine/config/oci/generate"
+	"github.com/sylabs/singularity/internal/pkg/util/passwdfile"
 )
 
 // RootFs is the default root path for OCI bundle
@@ -102,4 +105,19 @@ func DeleteBundle(bundlePath string) error {
 		return fmt.Errorf("failed to delete bundle %s directory: %s", bundlePath, err)
 	}
 	return nil
+}
+
+// BundleUser returns a user struct for the specified user, from the bundle passwd file.
+func Bundle(bundlePath, user string) (u *user.User, err error) {
+	passwd := filepath.Join(RootFs(bundlePath).Path(), "etc", "passwd")
+	if _, err := os.Stat(passwd); err != nil {
+		return nil, fmt.Errorf("cannot access container passwd file: %w", err)
+	}
+
+	// We have a numeric container uid
+	if _, err := strconv.Atoi(user); err == nil {
+		return passwdfile.LookupUserIDInFile(passwd, user)
+	}
+	// We have a container username
+	return passwdfile.LookupUserInFile(passwd, user)
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/pkg/runtime/launcher/oci/launcher_linux.go
@@ -26,6 +26,7 @@ import (
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher"
 	"github.com/apptainer/apptainer/internal/pkg/util/fs/files"
 	"github.com/apptainer/apptainer/internal/pkg/util/user"
+	"github.com/apptainer/apptainer/pkg/ocibundle"
 	"github.com/apptainer/apptainer/pkg/ocibundle/native"
 	"github.com/apptainer/apptainer/pkg/ocibundle/tools"
 	"github.com/apptainer/apptainer/pkg/syfs"
@@ -249,6 +250,27 @@ func (l *Launcher) createSpec() (*specs.Spec, error) {
 	return &spec, nil
 }
 
+func (l *Launcher) updateSpecFromImage(ctx context.Context, b ocibundle.Bundle, spec *specs.Spec, image string, process string, args []string) error {
+	imgSpec := b.ImageSpec()
+	if imgSpec == nil {
+		return fmt.Errorf("bundle has no image spec")
+	}
+
+	specProcess, err := l.getProcess(ctx, *imgSpec, image, b.Path(), process, args)
+	if err != nil {
+		return err
+	}
+	spec.Process = specProcess
+	if err := b.Update(ctx, spec); err != nil {
+		return err
+	}
+
+	if err := l.updatePasswdGroup(tools.RootFs(b.Path()).Path()); err != nil {
+		return err
+	}
+	return nil
+}
+
 func (l *Launcher) updatePasswdGroup(rootfs string) error {
 	uid := os.Getuid()
 	gid := os.Getgid()
@@ -350,18 +372,7 @@ func (l *Launcher) Exec(ctx context.Context, image string, process string, args 
 	}
 
 	// With reference to the bundle's image spec, now set the process configuration.
-	imgSpec := b.ImageSpec()
-	if imgSpec == nil {
-		return fmt.Errorf("bundle has no image spec")
-	}
-	specProcess, err := l.getProcess(ctx, *imgSpec, image, b.Path(), process, args)
-	if err != nil {
-		return err
-	}
-	spec.Process = specProcess
-	b.Update(ctx, spec)
-
-	if err := l.updatePasswdGroup(tools.RootFs(b.Path()).Path()); err != nil {
+	if err := l.updateSpecFromImage(ctx, b, spec, image, process, args); err != nil {
 		return err
 	}
 

pkg/ocibundle/tools/oci.go
@@ -12,11 +12,14 @@ package tools
 import (
 	"fmt"
 	"os"
+	"os/user"
 	"path/filepath"
+	"strconv"
 	"syscall"
 
 	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/config/oci"
 	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/config/oci/generate"
+	"github.com/apptainer/apptainer/internal/pkg/util/passwdfile"
 	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
@@ -106,4 +109,19 @@ func DeleteBundle(bundlePath string) error {
 		return fmt.Errorf("failed to delete bundle %s directory: %s", bundlePath, err)
 	}
 	return nil
+}
+
+// BundleUser returns a user struct for the specified user, from the bundle passwd file.
+func Bundle(bundlePath, user string) (u *user.User, err error) {
+	passwd := filepath.Join(RootFs(bundlePath).Path(), "etc", "passwd")
+	if _, err := os.Stat(passwd); err != nil {
+		return nil, fmt.Errorf("cannot access container passwd file: %w", err)
+	}
+
+	// We have a numeric container uid
+	if _, err := strconv.Atoi(user); err == nil {
+		return passwdfile.LookupUserIDInFile(passwd, user)
+	}
+	// We have a container username
+	return passwdfile.LookupUserInFile(passwd, user)
 }