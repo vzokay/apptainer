72b331c0a63a687cec25e3f68d339916076e8415
David Trudgian <david.trudgian@sylabs.io>
2023-06-16 14:24:31 +0100

    oci: support --containlibs / SINGULARITY_CONTAINLIBS
    Support the hidden `--containlibs` flag, and its env var
    `SINGULARITY_CONTAINLIBS` in `--oci` mode.
    
    Specified paths are bound into `/.singularity.d/libs` in the
    container, which is on the container `LD_LIBRARY_PATH`.
    
    Fixes sylabs/singularity#1782
    
    Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

--- e2e/actions/oci.go
+++ e2e/actions/oci.go
@@ -268,6 +268,14 @@ func (c actionTests) actionOciExec(t *testing.T) {
 				e2e.ExpectOutput(e2e.RegexMatch, `^(\s*)Server:(\s+)(1\.1\.1\.1)(\s*)\n`),
 			},
 		},
+		{
+			name: "Containlibs",
+			argv: []string{"--containlibs", "/etc/hosts", imageRef, "ls", "/.singularity.d/libs"},
+			exit: 0,
+			wantOutputs: []e2e.ApptainerCmdResultOp{
+				e2e.ExpectOutput(e2e.ExactMatch, `hosts`),
+			},
+		},
 	}
 	for _, profile := range e2e.OCIProfiles {
 		t.Run(profile.String(), func(t *testing.T) {
