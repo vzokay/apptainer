sylabs/singularity

fix rel. path --workdir with --scratch, add native and oci e2e tests

e2e/actions/actions.go
@@ -2492,6 +2492,48 @@ func (c actionTests) actionNoSIFFUSE(t *testing.T) {
 	}
 }
 
+// Make sure --workdir and --scratch work together nicely even when workdir is a
+// relative path. Test needs to be run in non-parallel mode, because it changes
+// the current working directory of the host.
+func (c actionTests) relWorkdirScratch(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	testdir, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "persistent-overlay-", "")
+	t.Cleanup(func() {
+		if !t.Failed() {
+			cleanup(t)
+		}
+	})
+
+	const subdirName string = "mysubdir"
+	if err := os.Mkdir(filepath.Join(testdir, subdirName), 0o777); err != nil {
+		t.Fatalf("could not create subdirectory %q in %q: %s", subdirName, testdir, err)
+	}
+
+	// Change current working directory, with deferred undoing of change.
+	prevCwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("could not get current working directory: %s", err)
+	}
+	defer os.Chdir(prevCwd)
+	if err = os.Chdir(testdir); err != nil {
+		t.Fatalf("could not change cwd to %q: %s", testdir, err)
+	}
+
+	profiles := []e2e.Profile{e2e.UserProfile, e2e.RootProfile, e2e.FakerootProfile, e2e.UserNamespaceProfile}
+
+	for _, p := range profiles {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs("--contain", "--workdir", "./"+subdirName, "--scratch", "/myscratch", c.env.ImagePath, "true"),
+			e2e.ExpectExit(0),
+		)
+	}
+}
+
 func countSquashfuseMounts(t *testing.T) int {
 	count := 0
 
@@ -2519,42 +2561,44 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	np := testhelper.NoParallel
 
 	return testhelper.Tests{
-		"action URI":                c.RunFromURI,              // action_URI
-		"exec":                      c.actionExec,              // singularity exec
-		"persistent overlay":        c.PersistentOverlay,       // Persistent Overlay
-		"persistent overlay unpriv": c.PersistentOverlayUnpriv, // Persistent Overlay Unprivileged
-		"run":                       c.actionRun,               // singularity run
-		"shell":                     c.actionShell,             // shell interaction
-		"STDPIPE":                   c.STDPipe,                 // stdin/stdout pipe
-		"action basic profiles":     c.actionBasicProfiles,     // run basic action under different profiles
-		"issue 4488":                c.issue4488,               // https://github.com/sylabs/singularity/issues/4488
-		"issue 4587":                c.issue4587,               // https://github.com/sylabs/singularity/issues/4587
-		"issue 4755":                c.issue4755,               // https://github.com/sylabs/singularity/issues/4755
-		"issue 4768":                c.issue4768,               // https://github.com/sylabs/singularity/issues/4768
-		"issue 4797":                c.issue4797,               // https://github.com/sylabs/singularity/issues/4797
-		"issue 4823":                c.issue4823,               // https://github.com/sylabs/singularity/issues/4823
-		"issue 4836":                c.issue4836,               // https://github.com/sylabs/singularity/issues/4836
-		"issue 5211":                c.issue5211,               // https://github.com/sylabs/singularity/issues/5211
-		"issue 5228":                c.issue5228,               // https://github.com/sylabs/singularity/issues/5228
-		"issue 5271":                c.issue5271,               // https://github.com/sylabs/singularity/issues/5271
-		"issue 5307":                c.issue5307,               // https://github.com/sylabs/singularity/issues/5307
-		"issue 5399":                c.issue5399,               // https://github.com/sylabs/singularity/issues/5399
-		"issue 5455":                c.issue5455,               // https://github.com/sylabs/singularity/issues/5455
-		"issue 5465":                c.issue5465,               // https://github.com/sylabs/singularity/issues/5465
-		"issue 5599":                c.issue5599,               // https://github.com/sylabs/singularity/issues/5599
-		"issue 5631":                c.issue5631,               // https://github.com/sylabs/singularity/issues/5631
-		"issue 5690":                c.issue5690,               // https://github.com/sylabs/singularity/issues/5690
-		"network":                   c.actionNetwork,           // test basic networking
-		"binds":                     c.actionBinds,             // test various binds with --bind and --mount
-		"exit and signals":          c.exitSignals,             // test exit and signals propagation
-		"fuse mount":                c.fuseMount,               // test fusemount option
-		"bind image":                c.bindImage,               // test bind image with --bind and --mount
-		"no-mount":                  c.actionNoMount,           // test --no-mount
-		"compat":                    np(c.actionCompat),        // test --compat
-		"umask":                     np(c.actionUmask),         // test umask propagation
-		"invalidRemote":             np(c.invalidRemote),       // GHSA-5mv9-q7fq-9394
-		"SIFFUSE":                   np(c.actionSIFFUSE),       // test --sif-fuse
-		"NoSIFFUSE":                 np(c.actionNoSIFFUSE),     // test absence of squashfs and CleanupHost()
+		"action URI":                c.RunFromURI,               // action_URI
+		"exec":                      c.actionExec,               // singularity exec
+		"persistent overlay":        c.PersistentOverlay,        // Persistent Overlay
+		"persistent overlay unpriv": c.PersistentOverlayUnpriv,  // Persistent Overlay Unprivileged
+		"run":                       c.actionRun,                // singularity run
+		"shell":                     c.actionShell,              // shell interaction
+		"STDPIPE":                   c.STDPipe,                  // stdin/stdout pipe
+		"action basic profiles":     c.actionBasicProfiles,      // run basic action under different profiles
+		"issue 4488":                c.issue4488,                // https://github.com/sylabs/singularity/issues/4488
+		"issue 4587":                c.issue4587,                // https://github.com/sylabs/singularity/issues/4587
+		"issue 4755":                c.issue4755,                // https://github.com/sylabs/singularity/issues/4755
+		"issue 4768":                c.issue4768,                // https://github.com/sylabs/singularity/issues/4768
+		"issue 4797":                c.issue4797,                // https://github.com/sylabs/singularity/issues/4797
+		"issue 4823":                c.issue4823,                // https://github.com/sylabs/singularity/issues/4823
+		"issue 4836":                c.issue4836,                // https://github.com/sylabs/singularity/issues/4836
+		"issue 5211":                c.issue5211,                // https://github.com/sylabs/singularity/issues/5211
+		"issue 5228":                c.issue5228,                // https://github.com/sylabs/singularity/issues/5228
+		"issue 5271":                c.issue5271,                // https://github.com/sylabs/singularity/issues/5271
+		"issue 5307":                c.issue5307,                // https://github.com/sylabs/singularity/issues/5307
+		"issue 5399":                c.issue5399,                // https://github.com/sylabs/singularity/issues/5399
+		"issue 5455":                c.issue5455,                // https://github.com/sylabs/singularity/issues/5455
+		"issue 5465":                c.issue5465,                // https://github.com/sylabs/singularity/issues/5465
+		"issue 5599":                c.issue5599,                // https://github.com/sylabs/singularity/issues/5599
+		"issue 5631":                c.issue5631,                // https://github.com/sylabs/singularity/issues/5631
+		"issue 5690":                c.issue5690,                // https://github.com/sylabs/singularity/issues/5690
+		"network":                   c.actionNetwork,            // test basic networking
+		"binds":                     c.actionBinds,              // test various binds with --bind and --mount
+		"exit and signals":          c.exitSignals,              // test exit and signals propagation
+		"fuse mount":                c.fuseMount,                // test fusemount option
+		"bind image":                c.bindImage,                // test bind image with --bind and --mount
+		"no-mount":                  c.actionNoMount,            // test --no-mount
+		"compat":                    np(c.actionCompat),         // test --compat
+		"umask":                     np(c.actionUmask),          // test umask propagation
+		"invalidRemote":             np(c.invalidRemote),        // GHSA-5mv9-q7fq-9394
+		"SIFFUSE":                   np(c.actionSIFFUSE),        // test --sif-fuse
+		"NoSIFFUSE":                 np(c.actionNoSIFFUSE),      // test absence of squashfs and CleanupHost()
+		"relWorkdirScratch":         np(c.relWorkdirScratch),    // test relative --workdir with --scratch
+		"ociRelWorkdirScratch":      np(c.ociRelWorkdirScratch), // test relative --workdir with --scratch in OCI mode
 		//
 		// OCI Runtime Mode
 		//

e2e/actions/oci.go
@@ -1169,3 +1169,46 @@ func countLines(path string) (int, error) {
 
 	return lines, nil
 }
+
+// Make sure --workdir and --scratch work together nicely even when workdir is a
+// relative path. Test needs to be run in non-parallel mode, because it changes
+// the current working directory of the host.
+func (c actionTests) ociRelWorkdirScratch(t *testing.T) {
+	e2e.EnsureOCIArchive(t, c.env)
+	imageRef := "oci-archive:" + c.env.OCIArchivePath
+
+	testdir, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "persistent-overlay-", "")
+	t.Cleanup(func() {
+		if !t.Failed() {
+			cleanup(t)
+		}
+	})
+
+	const subdirName string = "mysubdir"
+	if err := os.Mkdir(filepath.Join(testdir, subdirName), 0o777); err != nil {
+		t.Fatalf("could not create subdirectory %q in %q: %s", subdirName, testdir, err)
+	}
+
+	// Change current working directory, with deferred undoing of change.
+	prevCwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("could not get current working directory: %s", err)
+	}
+	defer os.Chdir(prevCwd)
+	if err = os.Chdir(testdir); err != nil {
+		t.Fatalf("could not change cwd to %q: %s", testdir, err)
+	}
+
+	profiles := e2e.OCIProfiles
+
+	for _, p := range profiles {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs("--workdir", "./"+subdirName, "--scratch", "/myscratch", imageRef, "true"),
+			e2e.ExpectExit(0),
+		)
+	}
+}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/actions.go
@@ -2842,6 +2842,48 @@ func (c actionTests) actionFakerootHome(t *testing.T) {
 	}
 }
 
+// Make sure --workdir and --scratch work together nicely even when workdir is a
+// relative path. Test needs to be run in non-parallel mode, because it changes
+// the current working directory of the host.
+func (c actionTests) relWorkdirScratch(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	testdir, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "persistent-overlay-", "")
+	t.Cleanup(func() {
+		if !t.Failed() {
+			cleanup(t)
+		}
+	})
+
+	const subdirName string = "mysubdir"
+	if err := os.Mkdir(filepath.Join(testdir, subdirName), 0o777); err != nil {
+		t.Fatalf("could not create subdirectory %q in %q: %s", subdirName, testdir, err)
+	}
+
+	// Change current working directory, with deferred undoing of change.
+	prevCwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("could not get current working directory: %s", err)
+	}
+	defer os.Chdir(prevCwd)
+	if err = os.Chdir(testdir); err != nil {
+		t.Fatalf("could not change cwd to %q: %s", testdir, err)
+	}
+
+	profiles := []e2e.Profile{e2e.UserProfile, e2e.RootProfile, e2e.FakerootProfile, e2e.UserNamespaceProfile}
+
+	for _, p := range profiles {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs("--contain", "--workdir", "./"+subdirName, "--scratch", "/myscratch", c.env.ImagePath, "true"),
+			e2e.ExpectExit(0),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2851,44 +2893,46 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	np := testhelper.NoParallel
 
 	return testhelper.Tests{
-		"action URI":                c.RunFromURI,              // action_URI
-		"singularity link":          c.singularityLink,         // singularity symlink
-		"exec":                      c.actionExec,              // apptainer exec
-		"persistent overlay":        c.PersistentOverlay,       // Persistent Overlay
-		"persistent overlay unpriv": c.PersistentOverlayUnpriv, // Persistent Overlay Unprivileged
-		"run":                       c.actionRun,               // apptainer run
-		"shell":                     c.actionShell,             // shell interaction
-		"STDPIPE":                   c.STDPipe,                 // stdin/stdout pipe
-		"action basic profiles":     c.actionBasicProfiles,     // run basic action under different profiles
-		"issue 4488":                c.issue4488,               // https://github.com/apptainer/singularity/issues/4488
-		"issue 4587":                c.issue4587,               // https://github.com/apptainer/singularity/issues/4587
-		"issue 4755":                c.issue4755,               // https://github.com/apptainer/singularity/issues/4755
-		"issue 4768":                c.issue4768,               // https://github.com/apptainer/singularity/issues/4768
-		"issue 4797":                c.issue4797,               // https://github.com/apptainer/singularity/issues/4797
-		"issue 4823":                c.issue4823,               // https://github.com/apptainer/singularity/issues/4823
-		"issue 4836":                c.issue4836,               // https://github.com/apptainer/singularity/issues/4836
-		"issue 5211":                c.issue5211,               // https://github.com/apptainer/singularity/issues/5211
-		"issue 5228":                c.issue5228,               // https://github.com/apptainer/singularity/issues/5228
-		"issue 5271":                c.issue5271,               // https://github.com/apptainer/singularity/issues/5271
-		"issue 5399":                c.issue5399,               // https://github.com/apptainer/singularity/issues/5399
-		"issue 5455":                c.issue5455,               // https://github.com/apptainer/singularity/issues/5455
-		"issue 5465":                c.issue5465,               // https://github.com/apptainer/singularity/issues/5465
-		"issue 5599":                c.issue5599,               // https://github.com/apptainer/singularity/issues/5599
-		"issue 5631":                c.issue5631,               // https://github.com/apptainer/singularity/issues/5631
-		"issue 5690":                c.issue5690,               // https://github.com/apptainer/singularity/issues/5690
-		"issue 6165":                c.issue6165,               // https://github.com/apptainer/singularity/issues/6165
-		"issue 619":                 c.issue619,                // https://github.com/apptainer/apptainer/issues/619
-		"network":                   c.actionNetwork,           // test basic networking
-		"binds":                     c.actionBinds,             // test various binds with --bind and --mount
-		"exit and signals":          c.exitSignals,             // test exit and signals propagation
-		"fuse mount":                c.fuseMount,               // test fusemount option
-		"bind image":                c.bindImage,               // test bind image with --bind and --mount
-		"unsquash":                  c.actionUnsquash,          // test --unsquash
-		"no-mount":                  c.actionNoMount,           // test --no-mount
-		"compat":                    np(c.actionCompat),        // test --compat
-		"umask":                     np(c.actionUmask),         // test umask propagation
-		"invalidRemote":             np(c.invalidRemote),       // GHSA-5mv9-q7fq-9394
-		"fakeroot home":             c.actionFakerootHome,      // test home dir in fakeroot
+		"action URI":                c.RunFromURI,               // action_URI
+		"singularity link":          c.singularityLink,          // singularity symlink
+		"exec":                      c.actionExec,               // apptainer exec
+		"persistent overlay":        c.PersistentOverlay,        // Persistent Overlay
+		"persistent overlay unpriv": c.PersistentOverlayUnpriv,  // Persistent Overlay Unprivileged
+		"run":                       c.actionRun,                // apptainer run
+		"shell":                     c.actionShell,              // shell interaction
+		"STDPIPE":                   c.STDPipe,                  // stdin/stdout pipe
+		"action basic profiles":     c.actionBasicProfiles,      // run basic action under different profiles
+		"issue 4488":                c.issue4488,                // https://github.com/apptainer/singularity/issues/4488
+		"issue 4587":                c.issue4587,                // https://github.com/apptainer/singularity/issues/4587
+		"issue 4755":                c.issue4755,                // https://github.com/apptainer/singularity/issues/4755
+		"issue 4768":                c.issue4768,                // https://github.com/apptainer/singularity/issues/4768
+		"issue 4797":                c.issue4797,                // https://github.com/apptainer/singularity/issues/4797
+		"issue 4823":                c.issue4823,                // https://github.com/apptainer/singularity/issues/4823
+		"issue 4836":                c.issue4836,                // https://github.com/apptainer/singularity/issues/4836
+		"issue 5211":                c.issue5211,                // https://github.com/apptainer/singularity/issues/5211
+		"issue 5228":                c.issue5228,                // https://github.com/apptainer/singularity/issues/5228
+		"issue 5271":                c.issue5271,                // https://github.com/apptainer/singularity/issues/5271
+		"issue 5399":                c.issue5399,                // https://github.com/apptainer/singularity/issues/5399
+		"issue 5455":                c.issue5455,                // https://github.com/apptainer/singularity/issues/5455
+		"issue 5465":                c.issue5465,                // https://github.com/apptainer/singularity/issues/5465
+		"issue 5599":                c.issue5599,                // https://github.com/apptainer/singularity/issues/5599
+		"issue 5631":                c.issue5631,                // https://github.com/apptainer/singularity/issues/5631
+		"issue 5690":                c.issue5690,                // https://github.com/apptainer/singularity/issues/5690
+		"issue 6165":                c.issue6165,                // https://github.com/apptainer/singularity/issues/6165
+		"issue 619":                 c.issue619,                 // https://github.com/apptainer/apptainer/issues/619
+		"network":                   c.actionNetwork,            // test basic networking
+		"binds":                     c.actionBinds,              // test various binds with --bind and --mount
+		"exit and signals":          c.exitSignals,              // test exit and signals propagation
+		"fuse mount":                c.fuseMount,                // test fusemount option
+		"bind image":                c.bindImage,                // test bind image with --bind and --mount
+		"unsquash":                  c.actionUnsquash,           // test --unsquash
+		"no-mount":                  c.actionNoMount,            // test --no-mount
+		"compat":                    np(c.actionCompat),         // test --compat
+		"umask":                     np(c.actionUmask),          // test umask propagation
+		"invalidRemote":             np(c.invalidRemote),        // GHSA-5mv9-q7fq-9394
+		"fakeroot home":             c.actionFakerootHome,       // test home dir in fakeroot
+		"relWorkdirScratch":         np(c.relWorkdirScratch),    // test relative --workdir with --scratch
+		"ociRelWorkdirScratch":      np(c.ociRelWorkdirScratch), // test relative --workdir with --scratch in OCI mode
 		//
 		// OCI Runtime Mode
 		//

e2e/actions/oci.go
@@ -1167,3 +1167,46 @@ func countLines(path string) (int, error) {
 
 	return lines, nil
 }
+
+// Make sure --workdir and --scratch work together nicely even when workdir is a
+// relative path. Test needs to be run in non-parallel mode, because it changes
+// the current working directory of the host.
+func (c actionTests) ociRelWorkdirScratch(t *testing.T) {
+	e2e.EnsureOCIArchive(t, c.env)
+	imageRef := "oci-archive:" + c.env.OCIArchivePath
+
+	testdir, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "persistent-overlay-", "")
+	t.Cleanup(func() {
+		if !t.Failed() {
+			cleanup(t)
+		}
+	})
+
+	const subdirName string = "mysubdir"
+	if err := os.Mkdir(filepath.Join(testdir, subdirName), 0o777); err != nil {
+		t.Fatalf("could not create subdirectory %q in %q: %s", subdirName, testdir, err)
+	}
+
+	// Change current working directory, with deferred undoing of change.
+	prevCwd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("could not get current working directory: %s", err)
+	}
+	defer os.Chdir(prevCwd)
+	if err = os.Chdir(testdir); err != nil {
+		t.Fatalf("could not change cwd to %q: %s", testdir, err)
+	}
+
+	profiles := e2e.OCIProfiles
+
+	for _, p := range profiles {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs("--workdir", "./"+subdirName, "--scratch", "/myscratch", imageRef, "true"),
+			e2e.ExpectExit(0),
+		)
+	}
+}