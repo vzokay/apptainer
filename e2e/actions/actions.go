sylabs/singularity

actions: Add --oci flag to call OCI launcher

e2e/actions/actions.go
@@ -903,7 +903,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1426,7 +1426,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2338,6 +2338,24 @@ func countSquashfuseMounts(t *testing.T) int {
 	return count
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2379,6 +2397,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"umask":                 c.actionUmask,         // test umask propagation
 		"no-mount":              c.actionNoMount,       // test --no-mount
 		"compat":                c.actionCompat,        // test --compat
+		"ociRuntime":            c.ociRuntime,          // test --oci (unimplemented)
 		"invalidRemote":         np(c.invalidRemote),   // GHSA-5mv9-q7fq-9394
 		"SIFFUSE":               np(c.actionSIFFUSE),   // test --sif-fuse
 		"NoSIFFUSE":             np(c.actionNoSIFFUSE), // test absence of squashfs and CleanupHost()

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/actions.go
@@ -929,7 +929,7 @@ func (c actionTests) actionBasicProfiles(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 
 		t.Run(profile.String(), func(t *testing.T) {
@@ -1850,7 +1850,7 @@ func (c actionTests) actionBinds(t *testing.T) {
 		},
 	}
 
-	for _, profile := range e2e.Profiles {
+	for _, profile := range e2e.NativeProfiles {
 		profile := profile
 		createWorkspaceDirs(t)
 
@@ -2861,6 +2861,24 @@ func (c actionTests) relWorkdirScratch(t *testing.T) {
 	}
 }
 
+func (c actionTests) ociRuntime(t *testing.T) {
+	e2e.EnsureImage(t, c.env)
+
+	for _, p := range []e2e.Profile{e2e.OCIUserProfile, e2e.OCIRootProfile} {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(c.env.ImagePath, "/bin/true"),
+			e2e.ExpectExit(
+				255,
+				e2e.ExpectError(e2e.ContainMatch, "not implemented"),
+			),
+		)
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := actionTests{
@@ -2906,6 +2924,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"no-mount":                  c.actionNoMount,           // test --no-mount
 		"compat":                    np(c.actionCompat),        // test --compat
 		"umask":                     np(c.actionUmask),         // test umask propagation
+		"ociRuntime":                c.ociRuntime,              // test --oci (unimplemented)
 		"invalidRemote":             np(c.invalidRemote),       // GHSA-5mv9-q7fq-9394
 		"fakeroot home":             c.actionFakerootHome,      // test home dir in fakeroot
 		"relWorkdirScratch":         np(c.relWorkdirScratch),   // test relative --workdir with --scratch