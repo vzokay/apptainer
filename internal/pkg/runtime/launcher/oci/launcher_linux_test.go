sylabs/singularity

test: oci: Test_addNamespaces

internal/pkg/runtime/launcher/oci/launcher_linux_test.go
@@ -10,10 +10,14 @@ import (
 	"testing"
 
 	"github.com/sylabs/singularity/internal/pkg/runtime/launcher"
+	"github.com/sylabs/singularity/internal/pkg/test"
 	"github.com/sylabs/singularity/pkg/util/singularityconf"
 )
 
 func TestNewLauncher(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
 	sc, err := singularityconf.GetConfig(nil)
 	if err != nil {
 		t.Fatalf("while initializing singularityconf: %s", err)

internal/pkg/runtime/launcher/oci/spec_linux_test.go
@@ -1,0 +1,68 @@
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package oci
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/sylabs/singularity/internal/pkg/runtime/launcher"
+	"github.com/sylabs/singularity/internal/pkg/test"
+)
+
+func Test_addNamespaces(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	tests := []struct {
+		name   string
+		ns     launcher.Namespaces
+		wantNS []specs.LinuxNamespace
+	}{
+		{
+			name:   "none",
+			ns:     launcher.Namespaces{},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "pid",
+			ns:     launcher.Namespaces{PID: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "ipc",
+			ns:     launcher.Namespaces{IPC: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "user",
+			ns:     launcher.Namespaces{User: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "net",
+			ns:     launcher.Namespaces{Net: true},
+			wantNS: append(defaultNamespaces, specs.LinuxNamespace{Type: specs.NetworkNamespace}),
+		},
+		{
+			name:   "uts",
+			ns:     launcher.Namespaces{UTS: true},
+			wantNS: append(defaultNamespaces, specs.LinuxNamespace{Type: specs.UTSNamespace}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			spec := minimalSpec()
+			newSpec := addNamespaces(spec, tt.ns)
+			newNS := newSpec.Linux.Namespaces
+			if !reflect.DeepEqual(newNS, tt.wantNS) {
+				t.Errorf("addNamespaces() got %v, want %v", newNS, tt.wantNS)
+			}
+		})
+	}
+}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

internal/pkg/runtime/launcher/oci/launcher_linux_test.go
@@ -14,10 +14,14 @@ import (
 	"testing"
 
 	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher"
+	"github.com/apptainer/apptainer/internal/pkg/test"
 	"github.com/apptainer/apptainer/pkg/util/apptainerconf"
 )
 
 func TestNewLauncher(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
 	sc, err := apptainerconf.GetConfig(nil)
 	if err != nil {
 		t.Fatalf("while initializing apptainerconf: %s", err)

internal/pkg/runtime/launcher/oci/spec_linux_test.go
@@ -1,0 +1,72 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package oci
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/apptainer/apptainer/internal/pkg/runtime/launcher"
+	"github.com/apptainer/apptainer/internal/pkg/test"
+	"github.com/opencontainers/runtime-spec/specs-go"
+)
+
+func Test_addNamespaces(t *testing.T) {
+	test.DropPrivilege(t)
+	defer test.ResetPrivilege(t)
+
+	tests := []struct {
+		name   string
+		ns     launcher.Namespaces
+		wantNS []specs.LinuxNamespace
+	}{
+		{
+			name:   "none",
+			ns:     launcher.Namespaces{},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "pid",
+			ns:     launcher.Namespaces{PID: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "ipc",
+			ns:     launcher.Namespaces{IPC: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "user",
+			ns:     launcher.Namespaces{User: true},
+			wantNS: defaultNamespaces,
+		},
+		{
+			name:   "net",
+			ns:     launcher.Namespaces{Net: true},
+			wantNS: append(defaultNamespaces, specs.LinuxNamespace{Type: specs.NetworkNamespace}),
+		},
+		{
+			name:   "uts",
+			ns:     launcher.Namespaces{UTS: true},
+			wantNS: append(defaultNamespaces, specs.LinuxNamespace{Type: specs.UTSNamespace}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			spec := minimalSpec()
+			newSpec := addNamespaces(spec, tt.ns)
+			newNS := newSpec.Linux.Namespaces
+			if !reflect.DeepEqual(newNS, tt.wantNS) {
+				t.Errorf("addNamespaces() got %v, want %v", newNS, tt.wantNS)
+			}
+		})
+	}
+}