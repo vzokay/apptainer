sylabs/singularity

oci: fix: ensure `oci exec` calls runc and e2e test checks this
As noted in review, the `runc` call for `oci exec` was not actually
being made. E2E test for `oci exec` was only checking return code, not
for output so it passed. Fix these issues.

In `e2e/suite.go` also move registry creation so that it is only
performed for the man tests in PID+mount NS. It is not needed for the
OCI/CGROUPS e2e tests and wastes CI time.

e2e/oci/oci.go
@@ -283,8 +283,9 @@ func (c ctx) testOciBasic(t *testing.T) {
 		t,
 		e2e.WithProfile(e2e.RootProfile),
 		e2e.WithCommand("oci exec"),
-		e2e.WithArgs(containerID, "id"),
-		e2e.ExpectExit(0),
+		e2e.WithArgs(containerID, "hostname"),
+		e2e.ExpectExit(0,
+			e2e.ExpectOutput(e2e.ContainMatch, "singularity")),
 	)
 
 	c.env.RunSingularity(

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/oci/oci.go
@@ -300,8 +300,9 @@ func (c ctx) testOciBasic(t *testing.T) {
 		t,
 		e2e.WithProfile(e2e.RootProfile),
 		e2e.WithCommand("oci exec"),
-		e2e.WithArgs(containerID, "id"),
-		e2e.ExpectExit(0),
+		e2e.WithArgs(containerID, "hostname"),
+		e2e.ExpectExit(0,
+			e2e.ExpectOutput(e2e.ContainMatch, "apptainer")),
 	)
 
 	c.env.RunApptainer(