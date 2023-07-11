sylabs/singularity

e2e: oci: add test for default CWD and $HOME in passwd line
The default cwd for a container should be $HOME, and $HOME should be
set correctly in `/etc/passwd`.

e2e/actions/oci.go
@@ -1780,3 +1780,47 @@ func (c actionTests) actionOciNoSetgroups(t *testing.T) {
 		),
 	)
 }
+
+// Check that by default, the container is entered at the correct $HOME for the
+// user, and $HOME in their passwd entry is correct.
+// https://github.com/sylabs/singularity/issues/1791
+func (c actionTests) actionOciHomeCwdPasswd(t *testing.T) {
+	e2e.EnsureOCIArchive(t, c.env)
+	imageRef := "oci-archive:" + c.env.OCIArchivePath
+	for _, p := range e2e.OCIProfiles {
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()+"/cwd"),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(imageRef, "pwd"),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.ExactMatch, p.ContainerUser(t).Dir),
+			),
+		)
+
+		cu := p.ContainerUser(t)
+		// Ignore shell field as we use preserve container value. Tested previously.
+		passwdLine := fmt.Sprintf("^%s:x:%d:%d:%s:%s:",
+			cu.Name,
+			cu.UID,
+			cu.GID,
+			cu.Gecos,
+			cu.Dir,
+		)
+
+		c.env.RunSingularity(
+			t,
+			e2e.AsSubtest(p.String()+"/passwd"),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(imageRef, "grep", "^"+cu.Name, "/etc/passwd"),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.RegexMatch, passwdLine),
+			),
+		)
+
+	}
+}

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/oci.go
@@ -1739,3 +1739,47 @@ func (c actionTests) actionOciNoMount(t *testing.T) {
 		)
 	}
 }
+
+// Check that by default, the container is entered at the correct $HOME for the
+// user, and $HOME in their passwd entry is correct.
+// https://github.com/sylabs/singularity/issues/1791
+func (c actionTests) actionOciHomeCwdPasswd(t *testing.T) {
+	e2e.EnsureOCIArchive(t, c.env)
+	imageRef := "oci-archive:" + c.env.OCIArchivePath
+	for _, p := range e2e.OCIProfiles {
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()+"/cwd"),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(imageRef, "pwd"),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.ExactMatch, p.ContainerUser(t).Dir),
+			),
+		)
+
+		cu := p.ContainerUser(t)
+		// Ignore shell field as we use preserve container value. Tested previously.
+		passwdLine := fmt.Sprintf("^%s:x:%d:%d:%s:%s:",
+			cu.Name,
+			cu.UID,
+			cu.GID,
+			cu.Gecos,
+			cu.Dir,
+		)
+
+		c.env.RunApptainer(
+			t,
+			e2e.AsSubtest(p.String()+"/passwd"),
+			e2e.WithProfile(p),
+			e2e.WithCommand("exec"),
+			e2e.WithArgs(imageRef, "grep", "^"+cu.Name, "/etc/passwd"),
+			e2e.ExpectExit(
+				0,
+				e2e.ExpectOutput(e2e.RegexMatch, passwdLine),
+			),
+		)
+
+	}
+}