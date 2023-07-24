sylabs/singularity

e2e: test --oci run/exec with args

e2e/actions/actions.go
@@ -2461,6 +2461,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun": c.actionOciRun, // singularity run --oci
+		"ociRun":  c.actionOciRun,  // singularity run --oci
+		"ociExec": c.actionOciExec, // singularity exec --oci
 	}
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/actions.go
@@ -2946,6 +2946,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun": c.actionOciRun, // apptainer run --oci
+		"ociRun":  c.actionOciRun,  // apptainer run --oci
+		"ociExec": c.actionOciExec, // apptainer exec --oci
 	}
 }