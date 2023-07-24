sylabs/singularity

e2e: minimal --oci namespace request tests

e2e/actions/actions.go
@@ -2461,8 +2461,9 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun":   c.actionOciRun,   // singularity run --oci
-		"ociExec":  c.actionOciExec,  // singularity exec --oci
-		"ociShell": c.actionOciShell, // singularity shell --oci
+		"ociRun":     c.actionOciRun,     // singularity run --oci
+		"ociExec":    c.actionOciExec,    // singularity exec --oci
+		"ociShell":   c.actionOciShell,   // singularity shell --oci
+		"ociNetwork": c.actionOciNetwork, // singularity exec --oci --net
 	}
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/actions/actions.go
@@ -2946,8 +2946,9 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// OCI Runtime Mode
 		//
-		"ociRun":   c.actionOciRun,   // apptainer run --oci
-		"ociExec":  c.actionOciExec,  // apptainer exec --oci
-		"ociShell": c.actionOciShell, // apptainer shell --oci
+		"ociRun":     c.actionOciRun,     // apptainer run --oci
+		"ociExec":    c.actionOciExec,    // apptainer exec --oci
+		"ociShell":   c.actionOciShell,   // apptainer shell --oci
+		"ociNetwork": c.actionOciNetwork, // apptainer exec --oci --net
 	}
 }