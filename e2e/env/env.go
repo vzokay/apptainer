sylabs/singularity

e2e: add SINGULARITYENV_ tests for --oci mode

e2e/env/env.go
@@ -643,6 +643,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// --oci mode
 		//
-		"oci environment option": c.ociEnvOption,
+		"oci environment singularityenv": c.ociSingularityEnv,
+		"oci environment option":         c.ociEnvOption,
 	}
 }

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

e2e/env/env.go
@@ -648,6 +648,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		//
 		// --oci mode
 		//
-		"oci environment option": c.ociEnvOption,
+		"oci environment apptainerenv": c.ociApptainerEnv,
+		"oci environment option":       c.ociEnvOption,
 	}
 }