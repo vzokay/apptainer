sylabs/singularity

oci: add oci-sif unit test, refactor other ocibundle tests
Add test to validate a bundle created from an oci-sif file.

Refactor common ocibundle test utility functions to separate package.

Add missing bundle deletion to native bundle tests.

pkg/ocibundle/native/bundle_linux_test.go
@@ -7,15 +7,13 @@ package native
 
 import (
 	"context"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"testing"
 
-	"github.com/opencontainers/runtime-tools/validate"
 	"github.com/sylabs/singularity/internal/pkg/cache"
+	"github.com/sylabs/singularity/internal/pkg/test"
+	ocitest "github.com/sylabs/singularity/internal/pkg/test/tool/oci"
 )
 
 const (
@@ -34,44 +32,15 @@ func setupCache(t *testing.T) *cache.Handle {
 	return h
 }
 
-func getTestTar(url string) (path string, err error) {
-	dl, err := os.CreateTemp("", "oci-test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dl.Close()
-
-	r, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer r.Body.Close()
-
-	_, err = io.Copy(dl, r.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return dl.Name(), nil
-}
-
-func validateBundle(t *testing.T, bundlePath string) {
-	v, err := validate.NewValidatorFromPath(bundlePath, false, "linux")
-	if err != nil {
-		t.Errorf("Could not create bundle validator: %v", err)
-	}
-	if err := v.CheckAll(); err != nil {
-		t.Errorf("Bundle not valid: %v", err)
-	}
-}
-
 func TestFromImageRef(t *testing.T) {
 	if testing.Short() {
 		t.SkipNow()
 	}
 
+	test.EnsurePrivilege(t)
+
 	// Prepare docker-archive source
-	dockerArchive, err := getTestTar(dockerArchiveURI)
+	dockerArchive, err := ocitest.GetTestImg(dockerArchiveURI)
 	if err != nil {
 		t.Fatalf("Could not download docker archive test file: %v", err)
 	}
@@ -90,7 +59,7 @@ func TestFromImageRef(t *testing.T) {
 		}
 	}
 	// Prepare oci-archive source
-	ociArchive, err := getTestTar(ociArchiveURI)
+	ociArchive, err := ocitest.GetTestImg(ociArchiveURI)
 	if err != nil {
 		t.Fatalf("Could not download oci archive test file: %v", err)
 	}
@@ -130,10 +99,14 @@ func TestFromImageRef(t *testing.T) {
 			}
 
 			if err := b.Create(context.Background(), nil); err != nil {
-				t.Fatalf("While creating bundle: %s", err)
+				t.Errorf("While creating bundle: %s", err)
 			}
 
-			validateBundle(t, bundleDir)
+			ocitest.ValidateBundle(t, bundleDir)
+
+			if err := b.Delete(context.Background()); err != nil {
+				t.Errorf("While deleting bundle: %s", err)
+			}
 		})
 	}
 }

pkg/ocibundle/ocisif/bundle_linux_test.go
@@ -1,0 +1,61 @@
+// Copyright (c) 2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package ocisif
+
+import (
+	"context"
+	"testing"
+
+	"github.com/sylabs/singularity/internal/pkg/test"
+	ocitest "github.com/sylabs/singularity/internal/pkg/test/tool/oci"
+)
+
+const (
+	ociSifURI = "https://s3.amazonaws.com/singularity-ci-public/alpine-oci-sif-squashfs.sif"
+)
+
+func TestFromImageRef(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	test.EnsurePrivilege(t)
+
+	// Prepare oci-sif source
+	ociSif, err := ocitest.GetTestImg(ociSifURI)
+	if err != nil {
+		t.Fatalf("Could not download oci-sif test file: %v", err)
+	}
+
+	tests := []struct {
+		name     string
+		imageRef string
+	}{
+		{"oci-sif", "oci-sif:" + ociSif},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bundleDir := t.TempDir()
+			b, err := New(
+				OptBundlePath(bundleDir),
+				OptImageRef(tt.imageRef),
+			)
+			if err != nil {
+				t.Fatalf("While initializing bundle: %s", err)
+			}
+
+			if err := b.Create(context.Background(), nil); err != nil {
+				t.Errorf("While creating bundle: %s", err)
+			}
+
+			ocitest.ValidateBundle(t, bundleDir)
+
+			if err := b.Delete(context.Background()); err != nil {
+				t.Errorf("While deleting bundle: %s", err)
+			}
+		})
+	}
+}

pkg/ocibundle/sif/bundle_linux_test.go
@@ -11,9 +11,9 @@ import (
 	"runtime"
 	"testing"
 
-	"github.com/opencontainers/runtime-tools/validate"
 	"github.com/sylabs/singularity/internal/pkg/runtime/engine/config/oci"
 	"github.com/sylabs/singularity/internal/pkg/test"
+	ocitest "github.com/sylabs/singularity/internal/pkg/test/tool/oci"
 	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/pkg/ocibundle/tools"
 	"github.com/sylabs/singularity/pkg/util/fs/proc"
@@ -82,15 +82,7 @@ func TestFromSif(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			// Validate the bundle using OCI runtime-tools
-			// Run in non-host-specific mode. Our bundle is for the "linux" platform
-			v, err := validate.NewValidatorFromPath(bundlePath, false, "linux")
-			if err != nil {
-				t.Errorf("Could not create bundle validator: %v", err)
-			}
-			if err := v.CheckAll(); err != nil {
-				t.Errorf("Bundle not valid: %v", err)
-			}
+			ocitest.ValidateBundle(t, bundle.Path())
 
 			// Clean up
 			if err := bundle.Delete(context.Background()); err != nil {

vzokay/apptainer

Signed-off-by: Edita Kizinevic <edita.kizinevic@cern.ch>

pkg/ocibundle/native/bundle_linux_test.go
@@ -11,15 +11,13 @@ package native
 
 import (
 	"context"
-	"io"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"testing"
 
 	"github.com/apptainer/apptainer/internal/pkg/cache"
-	"github.com/opencontainers/runtime-tools/validate"
+	"github.com/apptainer/apptainer/internal/pkg/test"
+	ocitest "github.com/apptainer/apptainer/internal/pkg/test/tool/oci"
 )
 
 const (
@@ -38,44 +36,15 @@ func setupCache(t *testing.T) *cache.Handle {
 	return h
 }
 
-func getTestTar(url string) (path string, err error) {
-	dl, err := os.CreateTemp("", "oci-test")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dl.Close()
-
-	r, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer r.Body.Close()
-
-	_, err = io.Copy(dl, r.Body)
-	if err != nil {
-		return "", err
-	}
-
-	return dl.Name(), nil
-}
-
-func validateBundle(t *testing.T, bundlePath string) {
-	v, err := validate.NewValidatorFromPath(bundlePath, false, "linux")
-	if err != nil {
-		t.Errorf("Could not create bundle validator: %v", err)
-	}
-	if err := v.CheckAll(); err != nil {
-		t.Errorf("Bundle not valid: %v", err)
-	}
-}
-
 func TestFromImageRef(t *testing.T) {
 	if testing.Short() {
 		t.SkipNow()
 	}
 
+	test.EnsurePrivilege(t)
+
 	// Prepare docker-archive source
-	dockerArchive, err := getTestTar(dockerArchiveURI)
+	dockerArchive, err := ocitest.GetTestImg(dockerArchiveURI)
 	if err != nil {
 		t.Fatalf("Could not download docker archive test file: %v", err)
 	}
@@ -94,7 +63,7 @@ func TestFromImageRef(t *testing.T) {
 		}
 	}
 	// Prepare oci-archive source
-	ociArchive, err := getTestTar(ociArchiveURI)
+	ociArchive, err := ocitest.GetTestImg(ociArchiveURI)
 	if err != nil {
 		t.Fatalf("Could not download oci archive test file: %v", err)
 	}
@@ -134,10 +103,14 @@ func TestFromImageRef(t *testing.T) {
 			}
 
 			if err := b.Create(context.Background(), nil); err != nil {
-				t.Fatalf("While creating bundle: %s", err)
+				t.Errorf("While creating bundle: %s", err)
 			}
 
-			validateBundle(t, bundleDir)
+			ocitest.ValidateBundle(t, bundleDir)
+
+			if err := b.Delete(context.Background()); err != nil {
+				t.Errorf("While deleting bundle: %s", err)
+			}
 		})
 	}
 }

pkg/ocibundle/ocisif/bundle_linux_test.go
@@ -1,0 +1,65 @@
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
+//   For website terms of use, trademark policy, privacy policy and other
+//   project policies see https://lfprojects.org/policies
+// Copyright (c) 2023, Sylabs Inc. All rights reserved.
+// This software is licensed under a 3-clause BSD license. Please consult the
+// LICENSE.md file distributed with the sources of this project regarding your
+// rights to use or distribute this software.
+
+package ocisif
+
+import (
+	"context"
+	"testing"
+
+	"github.com/apptainer/apptainer/internal/pkg/test"
+	ocitest "github.com/apptainer/apptainer/internal/pkg/test/tool/oci"
+)
+
+const (
+	ociSifURI = "https://s3.amazonaws.com/singularity-ci-public/alpine-oci-sif-squashfs.sif"
+)
+
+func TestFromImageRef(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	test.EnsurePrivilege(t)
+
+	// Prepare oci-sif source
+	ociSif, err := ocitest.GetTestImg(ociSifURI)
+	if err != nil {
+		t.Fatalf("Could not download oci-sif test file: %v", err)
+	}
+
+	tests := []struct {
+		name     string
+		imageRef string
+	}{
+		{"oci-sif", "oci-sif:" + ociSif},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			bundleDir := t.TempDir()
+			b, err := New(
+				OptBundlePath(bundleDir),
+				OptImageRef(tt.imageRef),
+			)
+			if err != nil {
+				t.Fatalf("While initializing bundle: %s", err)
+			}
+
+			if err := b.Create(context.Background(), nil); err != nil {
+				t.Errorf("While creating bundle: %s", err)
+			}
+
+			ocitest.ValidateBundle(t, bundleDir)
+
+			if err := b.Delete(context.Background()); err != nil {
+				t.Errorf("While deleting bundle: %s", err)
+			}
+		})
+	}
+}

pkg/ocibundle/sif/bundle_linux_test.go
@@ -17,10 +17,10 @@ import (
 
 	"github.com/apptainer/apptainer/internal/pkg/runtime/engine/config/oci"
 	"github.com/apptainer/apptainer/internal/pkg/test"
+	ocitest "github.com/apptainer/apptainer/internal/pkg/test/tool/oci"
 	"github.com/apptainer/apptainer/internal/pkg/util/fs"
 	"github.com/apptainer/apptainer/pkg/ocibundle/tools"
 	"github.com/apptainer/apptainer/pkg/util/fs/proc"
-	"github.com/opencontainers/runtime-tools/validate"
 )
 
 // We need a busybox SIF for these tests. We used to download it each time, but we have one
@@ -86,15 +86,7 @@ func TestFromSif(t *testing.T) {
 				t.Fatal(err)
 			}
 
-			// Validate the bundle using OCI runtime-tools
-			// Run in non-host-specific mode. Our bundle is for the "linux" platform
-			v, err := validate.NewValidatorFromPath(bundlePath, false, "linux")
-			if err != nil {
-				t.Errorf("Could not create bundle validator: %v", err)
-			}
-			if err := v.CheckAll(); err != nil {
-				t.Errorf("Bundle not valid: %v", err)
-			}
+			ocitest.ValidateBundle(t, bundle.Path())
 
 			// Clean up
 			if err := bundle.Delete(context.Background()); err != nil {